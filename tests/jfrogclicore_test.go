bc4f97c5b4ae7998d9b80ac47aa7dba4d64f0e0a
Yahav Itzhak <yahavi@users.noreply.github.com>
2020-12-27 09:27:09 +0200

    Improve conf tests (#53)

--- tests/jfrogclicore_test.go
+++ tests/jfrogclicore_test.go
@@ -1,36 +1,29 @@
 package tests
 
 import (
+	"os"
+	"testing"
+
 	"github.com/jfrog/jfrog-cli-core/utils/coreutils"
 	"github.com/jfrog/jfrog-cli-core/utils/tests"
 	"github.com/jfrog/jfrog-client-go/utils/log"
 	clientTests "github.com/jfrog/jfrog-client-go/utils/tests"
-	"os"
-	"path/filepath"
-	"testing"
 )
 
 const (
-	JfrogTestsHome       = ".jfrogCliCoreTest"
 	CoreIntegrationTests = "github.com/jfrog/jfrog-cli-core/tests"
 )
 
 func TestUnitTests(t *testing.T) {
-	homePath, err := filepath.Abs(JfrogTestsHome)
-	if err != nil {
-		log.Error(err)
-		os.Exit(1)
-	}
-
-	oldHome, err := tests.SetJfrogHome(homePath)
+	oldHome, err := tests.SetJfrogHome()
 	if err != nil {
 		log.Error(err)
 		os.Exit(1)
 	}
 	defer os.Setenv(coreutils.HomeDir, oldHome)
+	defer tests.CleanUnitTestsJfrogHome()
 
 	packages := clientTests.GetTestPackages("./../...")
 	packages = clientTests.ExcludeTestsPackage(packages, CoreIntegrationTests)
 	clientTests.RunTests(packages, false)
-	tests.CleanUnitTestsJfrogHome(homePath)
 }

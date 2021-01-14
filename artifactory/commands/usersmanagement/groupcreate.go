96f927623a2eef1f0b1cc7d81c1d1107a0230d8b
Tal Arian <tala@jfrog.com>
2021-01-14 20:28:16 +0200

    Add new group create and update commands

---
+++ artifactory/commands/usersmanagement/groupcreate.go
@@ -0,0 +1,51 @@
+package usersmanagement
+
+import (
+	"github.com/jfrog/jfrog-cli-core/artifactory/utils"
+	"github.com/jfrog/jfrog-cli-core/utils/config"
+	"github.com/jfrog/jfrog-client-go/artifactory/services"
+)
+
+type GroupCreateCommand struct {
+	rtDetails *config.ArtifactoryDetails
+	name      string
+}
+
+func NewGroupCreateCommand() *GroupCreateCommand {
+	return &GroupCreateCommand{}
+}
+
+func (gcc *GroupCreateCommand) RtDetails() (*config.ArtifactoryDetails, error) {
+	return gcc.rtDetails, nil
+}
+
+func (gcc *GroupCreateCommand) SetRtDetails(rtDetails *config.ArtifactoryDetails) *GroupCreateCommand {
+	gcc.rtDetails = rtDetails
+	return gcc
+}
+
+func (gcc *GroupCreateCommand) SetName(groupName string) *GroupCreateCommand {
+	gcc.name = groupName
+	return gcc
+}
+
+func (gcc *GroupCreateCommand) Name() string {
+	return gcc.name
+}
+
+func (gcc *GroupCreateCommand) CommandName() string {
+	return "rt_group_create"
+}
+
+func (gcc *GroupCreateCommand) Run() error {
+	servicesManager, err := utils.CreateServiceManager(gcc.rtDetails, false)
+	if err != nil {
+		return err
+	}
+	group := new(services.Group)
+	group.Name = gcc.Name()
+
+	servicesManager.CreateGroup(*group)
+
+	return err
+}
---
+++ artifactory/commands/usersmanagement/groupupdate.go
@@ -0,0 +1,62 @@
+package usersmanagement
+
+import (
+	"github.com/jfrog/jfrog-cli-core/artifactory/utils"
+	"github.com/jfrog/jfrog-cli-core/utils/config"
+	"github.com/jfrog/jfrog-client-go/artifactory/services"
+)
+
+type GroupUpdateCommand struct {
+	rtDetails *config.ArtifactoryDetails
+	name      string
+	users     []string
+}
+
+func NewGroupUpdateCommand() *GroupUpdateCommand {
+	return &GroupUpdateCommand{}
+}
+
+func (guc *GroupUpdateCommand) RtDetails() (*config.ArtifactoryDetails, error) {
+	return guc.rtDetails, nil
+}
+
+func (guc *GroupUpdateCommand) SetRtDetails(rtDetails *config.ArtifactoryDetails) *GroupUpdateCommand {
+	guc.rtDetails = rtDetails
+	return guc
+}
+
+func (guc *GroupUpdateCommand) SetName(groupName string) *GroupUpdateCommand {
+	guc.name = groupName
+	return guc
+}
+
+func (guc *GroupUpdateCommand) Name() string {
+	return guc.name
+}
+
+func (guc *GroupUpdateCommand) SetUsers(users []string) *GroupUpdateCommand {
+	guc.users = users
+	return guc
+}
+
+func (guc *GroupUpdateCommand) Users() []string {
+	return guc.users
+}
+
+func (guc *GroupUpdateCommand) CommandName() string {
+	return "rt_group_update"
+}
+
+func (guc *GroupUpdateCommand) Run() error {
+	servicesManager, err := utils.CreateServiceManager(guc.rtDetails, false)
+	if err != nil {
+		return err
+	}
+	group := new(services.Group)
+	group.Name = guc.Name()
+	group.UsersNames = guc.Users()
+
+	servicesManager.UpdateGroup(*group)
+
+	return err
+}

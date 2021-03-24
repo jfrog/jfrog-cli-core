3c9dd6dd1c67a4b45ce6230adc23d51974dc09cf
Tal Arian <tala@jfrog.com>
2021-03-24 12:46:40 +0200

    Added trim to ScanFromConsole func

--- utils/ioutils/ioutils.go
+++ utils/ioutils/ioutils.go
@@ -52,6 +52,8 @@ func ScanFromConsole(caption string, scanInto *string, defaultValue string) {
 	if *scanInto == "" {
 		*scanInto = defaultValue
 	}
+	*scanInto = strings.TrimSpace(*scanInto)
+
 }
 
 func CopyFile(src, dst string, fileMode os.FileMode) error {

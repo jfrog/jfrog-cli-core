32b6f8e314b2fe2ee6e561738cb25290559a50db
gailazar300 <gail@jfrog.com>
2021-07-13 13:21:52 +0300

    change buildinfo version

--- artifactory/commands/mvn/mvn.go
+++ artifactory/commands/mvn/mvn.go
@@ -21,7 +21,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-const mavenExtractorDependencyVersion = "2.28.5"
+const mavenExtractorDependencyVersion = "2.28.6"
 
 // Deprecated. This version is the latest published in JCenter.
 const mavenExtractorDependencyJCenterVersion = "2.23.0"

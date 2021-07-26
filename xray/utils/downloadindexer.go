5e3950f2a0d81cef25dfb2c407e42cc73af0ab76
Asaf Gabai <asafg@jfrog.com>
2021-07-26 17:27:00 +0300

    The downloaded Indexer's version is requested from the Indexer itself. When a new Indexer is downloaded, the old ones are deleted (all but the two newest ones).

--- xray/utils/downloadindexer.go
+++ xray/utils/downloadindexer.go
@@ -3,11 +3,15 @@ package utils
 import (
 	"errors"
 	"fmt"
+	"github.com/jfrog/gofrog/io"
+	"github.com/jfrog/jfrog-cli-core/v2/utils/coreutils"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
 	"runtime"
+	"sort"
+	"strings"
 
 	"github.com/jfrog/jfrog-cli-core/v2/utils/config"
 	"github.com/jfrog/jfrog-client-go/http/httpclient"
@@ -21,6 +25,8 @@ import (
 const (
 	indexerFileName     = "indexer-app"
 	graphScanMinVersion = "3.29.0"
+	indexerDirName      = "xray-indexer"
+	tempIndexerDirName  = "temp"
 )
 
 func DownloadIndexerIfNeeded(xrayManager *xray.XrayServicesManager) (string, error) {
@@ -38,8 +44,8 @@ func DownloadIndexerIfNeeded(xrayManager *xray.XrayServicesManager) (string, err
 	if err != nil {
 		return "", err
 	}
-	downloadDirPath := filepath.Join(dependenciesPath, "xray-indexer", xrayVersionStr)
-	indexerPath := filepath.Join(downloadDirPath, indexerFileName)
+	indexerDirPath := filepath.Join(dependenciesPath, indexerDirName)
+	indexerPath := filepath.Join(indexerDirPath, xrayVersionStr, indexerFileName)
 	exists, err := fileutils.IsFileExists(indexerPath, false)
 	if err != nil {
 		return "", err
@@ -49,33 +55,115 @@ func DownloadIndexerIfNeeded(xrayManager *xray.XrayServicesManager) (string, err
 	}
 
 	log.Info("JFrog Xray Indexer is not cached locally. Downloading it now...")
-	err = downloadIndexer(xrayManager, downloadDirPath)
+	return downloadIndexer(xrayManager, indexerDirPath)
+}
+
+func downloadIndexer(xrayManager *xray.XrayServicesManager, indexerDirPath string) (string, error) {
+	tempDirPath := filepath.Join(indexerDirPath, tempIndexerDirName)
+
+	// Delete the temporary directory if it exists
+	tempDirExists, err := fileutils.IsDirExists(tempDirPath, false)
 	if err != nil {
 		return "", err
 	}
-	// Add execution premissions to the indexer
-	err = os.Chmod(indexerPath, 0777)
-	return indexerPath, err
-}
+	if tempDirExists {
+		err = os.RemoveAll(tempDirPath)
+		if err != nil {
+			return "", errorutils.CheckError(errors.New(fmt.Sprintf("Temporary download directory already exists, and can't be removed: %s\nRemove this directory manually and try again: %s", err.Error(), tempDirPath)))
+		}
+	}
 
-func downloadIndexer(xrayManager *xray.XrayServicesManager, downloadDirPath string) error {
+	// Delete all old indexers, but the two newest
+	err = deleteOldIndexers(indexerDirPath)
+	if err != nil {
+		return "", err
+	}
+
+	// Download the indexer from Xray to the temporary directory
 	url := fmt.Sprintf("%sapi/v1/indexer-resources/download/%s/%s", xrayManager.Config().GetServiceDetails().GetUrl(), runtime.GOOS, runtime.GOARCH)
 	downloadFileDetails := &httpclient.DownloadFileDetails{
 		DownloadPath:  url,
-		LocalPath:     downloadDirPath,
+		LocalPath:     tempDirPath,
 		LocalFileName: indexerFileName,
 	}
 	httpClientDetails := xrayManager.Config().GetServiceDetails().CreateHttpClientDetails()
-
 	resp, err := xrayManager.Client().DownloadFile(downloadFileDetails, "", &httpClientDetails, false)
 	if err == nil && resp.StatusCode != http.StatusOK {
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			return errorutils.CheckError(errors.New(fmt.Sprintf("%s received when attempting to download %s. An error occurred while trying to read the body of the response: %s", resp.Status, url, err.Error())))
+			return "", errorutils.CheckError(errors.New(fmt.Sprintf("%s received when attempting to download %s. An error occurred while trying to read the body of the response: %s", resp.Status, url, err.Error())))
 		}
 		resp.Body.Close()
-		err = errorutils.CheckError(errors.New(fmt.Sprintf("%s received when attempting to download %s\n%s", resp.Status, url, body)))
+		return "", errorutils.CheckError(errors.New(fmt.Sprintf("%s received when attempting to download %s\n%s", resp.Status, url, body)))
 	}
 
-	return err
+	// Add execution permissions to the indexer
+	indexerPath := filepath.Join(tempDirPath, indexerFileName)
+	err = os.Chmod(indexerPath, 0777)
+
+	indexerVersion, err := getIndexerVersion(indexerPath)
+	if err != nil {
+		return "", err
+	}
+	newDirPath := filepath.Join(indexerDirPath, indexerVersion)
+
+	// In case of a hot upgrade of Xray in progress, the version of the downloaded indexer might be different of the Xray version we got above,
+	// so the indexer we just downloaded may already exist.
+	newDirExists, err := fileutils.IsDirExists(newDirPath, false)
+	if err != nil {
+		return "", err
+	}
+	if newDirExists {
+		err = os.RemoveAll(tempDirPath)
+	} else {
+		err = os.Rename(tempDirPath, newDirPath)
+	}
+
+	return filepath.Join(newDirPath, indexerFileName), errorutils.CheckError(err)
+}
+
+func getIndexerVersion(indexerPath string) (string, error) {
+	indexCmd := &coreutils.GeneralExecCmd{
+		ExecPath: indexerPath,
+		Command:  []string{"version"},
+	}
+	output, err := io.RunCmdOutput(indexCmd)
+	if err != nil {
+		return "", errorutils.CheckError(err)
+	}
+	splitOutput := strings.Split(output, " ")
+	// The output of the command looks like: jfrog xray indexer-app version 1.2.3
+	indexerVersion := strings.TrimSuffix(splitOutput[len(splitOutput)-1], "\n")
+	return indexerVersion, nil
+}
+
+func deleteOldIndexers(indexerDirPath string) error {
+	filesList, err := ioutil.ReadDir(indexerDirPath)
+	if err != nil {
+		return errorutils.CheckError(err)
+	}
+	var dirsList []string
+	for _, file := range filesList {
+		if file.IsDir() {
+			dirsList = append(dirsList, file.Name())
+		}
+	}
+
+	if len(dirsList) <= 2 {
+		return nil
+	}
+
+	sort.Slice(dirsList, func(i, j int) bool {
+		currVersion := version.NewVersion(dirsList[i])
+		return currVersion.AtLeast(dirsList[j])
+	})
+
+	for i := 2; i < len(dirsList); i++ {
+		err = os.RemoveAll(filepath.Join(indexerDirPath, dirsList[i]))
+		if err != nil {
+			return errorutils.CheckError(err)
+		}
+	}
+
+	return nil
 }

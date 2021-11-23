2999e996bba24aa420b2cf44c812638e3edf2090
sverdlov93 <sverdlov93@gmail.com>
2021-11-23 14:30:37 +0200

    add docker scan
    Signed-off-by: sverdlov93 <sverdlov93@gmail.com>

--- xray/commands/scan/container.go
+++ xray/commands/scan/container.go
@@ -1,12 +1,16 @@
-package audit
+package scan
 
 import (
 	"errors"
 	"github.com/jfrog/jfrog-cli-core/v2/artifactory/utils/container"
 	"github.com/jfrog/jfrog-cli-core/v2/common/spec"
+	"github.com/jfrog/jfrog-client-go/utils/errorutils"
 	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
+	"os"
 	"path/filepath"
 )
+
+const indexerEnvPrefix = "JFROG_INDEXER_"
 
 type ContainerScanCommand struct {
 	ScanCommand
@@ -59,9 +63,65 @@ func (csc *ContainerScanCommand) Run() (err error) {
 		Pattern(tarFilePath).
 		BuildSpec()
 	csc.SetSpec(filSpec).SetThreads(1)
+
+	err = csc.setCredentialEnvsForIndexerApp()
+	if err != nil {
+		return errorutils.CheckError(err)
+	}
+	defer func() {
+		e := csc.unsetCredentialEnvsForIndexerApp()
+		if err == nil {
+			err = errorutils.CheckError(e)
+		}
+	}()
 	return csc.ScanCommand.Run()
 }
 
+// when indexing docker rpm files the indexer app needs connection with Xray Server to deal with the rpm files
+func (csc *ContainerScanCommand) setCredentialEnvsForIndexerApp() error {
+	err := os.Setenv(indexerEnvPrefix+"XRAY_URL", csc.serverDetails.XrayUrl)
+	if err != nil {
+		return err
+	}
+	if csc.serverDetails.AccessToken != "" {
+		err = os.Setenv(indexerEnvPrefix+"XRAY_ACCESS_TOKEN", csc.serverDetails.AccessToken)
+		if err != nil {
+			return err
+		}
+	} else {
+		err = os.Setenv(indexerEnvPrefix+"XRAY_USER", csc.serverDetails.User)
+		if err != nil {
+			return err
+		}
+		err = os.Setenv(indexerEnvPrefix+"XRAY_PASSWORD", csc.serverDetails.Password)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (csc *ContainerScanCommand) unsetCredentialEnvsForIndexerApp() error {
+	err := os.Unsetenv(indexerEnvPrefix + "XRAY_URL")
+	if err != nil {
+		return err
+	}
+	err = os.Unsetenv(indexerEnvPrefix + "XRAY_ACCESS_TOKEN")
+	if err != nil {
+		return err
+	}
+	err = os.Unsetenv(indexerEnvPrefix + "XRAY_USER")
+	if err != nil {
+		return err
+	}
+	err = os.Unsetenv(indexerEnvPrefix + "XRAY_PASSWORD")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (csc *ContainerScanCommand) CommandName() string {
-	return "xr_scan_container"
+	return "xr_container_scan"
 }

986bbdcbdfaf463d772f9c4a1f663c2bdd291ffd
Tal Arian <tala@jfrog.com>
2021-11-29 18:14:48 +0200

    Add New Env Setup Command (#260)
    * Add new JFrog setup logic
    
    * Add progress indication to new setup command
    
    * Use the new PollingExecutor utility
    
    * Update go.mod & go.sum

---
+++ general/envsetup/envsetup.go
@@ -0,0 +1,161 @@
+package envsetup
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jfrog/jfrog-cli-core/v2/common/commands"
+	"github.com/jfrog/jfrog-cli-core/v2/utils/config"
+	"github.com/jfrog/jfrog-client-go/http/httpclient"
+	clientutils "github.com/jfrog/jfrog-client-go/utils"
+	"github.com/jfrog/jfrog-client-go/utils/errorutils"
+	ioUtils "github.com/jfrog/jfrog-client-go/utils/io"
+	"github.com/jfrog/jfrog-client-go/utils/io/httputils"
+	"github.com/jfrog/jfrog-client-go/utils/log"
+
+	"github.com/pkg/browser"
+)
+
+const (
+	myJfrogEndPoint   = "https://myjfrog-api.jfrog.com/api/v1/activation/cloud/cli/getStatus/"
+	syncSleepInterval = 5 * time.Second  // 5 seconds
+	maxWaitMinutes    = 10 * time.Minute // 10 minutes
+)
+
+type EnvSetupCommand struct {
+	registrationURL string
+	id              uuid.UUID
+	serverDetails   *config.ServerDetails
+	progress        ioUtils.ProgressMgr
+}
+
+func (ftc *EnvSetupCommand) ServerDetails() (*config.ServerDetails, error) {
+	return nil, nil
+}
+
+func (ftc *EnvSetupCommand) SetProgress(progress ioUtils.ProgressMgr) {
+	ftc.progress = progress
+}
+
+func NewEnvSetupCommand(url string) *EnvSetupCommand {
+	return &EnvSetupCommand{
+		registrationURL: url,
+		id:              uuid.New(),
+	}
+}
+
+func (ftc *EnvSetupCommand) Run() (err error) {
+	browser.OpenURL(ftc.registrationURL + "?id=" + ftc.id.String())
+	ftc.progress.SetHeadlineMsg("Thank you for installing JFrog CLI! To complete your JFrog environment setup, please fill out the details in your browser")
+	server, err := ftc.getNewServerDetails()
+	if err != nil {
+		return
+	}
+	err = configServer(server)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Your new JFrog environment is ready!")
+	fmt.Println("	1. CD into your code project directory")
+	fmt.Println("	2. Run \"jf project init\"")
+	fmt.Println("	3. Read more about how to get started at https://...")
+	return nil
+}
+
+func (ftc *EnvSetupCommand) CommandName() string {
+	return "setup"
+}
+
+// Returns the new server deatailes from My-JFrog
+func (ftc *EnvSetupCommand) getNewServerDetails() (serverDetails *config.ServerDetails, err error) {
+	requestBody := &myJfrogGetStatusRequest{CliRegistrationId: ftc.id.String()}
+	requestContent, err := json.Marshal(requestBody)
+	if errorutils.CheckError(err) != nil {
+		return nil, err
+	}
+
+	httpClientDetails := httputils.HttpClientDetails{
+		Headers: map[string]string{"Content-Type": "application/json"},
+	}
+	client, err := httpclient.ClientBuilder().Build()
+	if err != nil {
+		return nil, err
+	}
+	message := fmt.Sprintf("Sync: Get MyJFrog status report. Request ID:%s...", ftc.id)
+
+	pollingAction := func() (shouldStop bool, responseBody []byte, err error) {
+		log.Debug(message)
+		resp, body, err := client.SendPost(myJfrogEndPoint, requestContent, httpClientDetails, "")
+		if err != nil {
+			return true, nil, err
+		}
+		if err = errorutils.CheckResponseStatus(resp, http.StatusOK, http.StatusNotFound); err != nil {
+			err = errorutils.CheckError(errorutils.GenerateResponseError(resp.Status, clientutils.IndentJson(body)))
+			return true, nil, err
+		}
+		log.Debug(message)
+		// Wait for 'ready=true' response from MyJFrog
+		if resp.StatusCode == http.StatusOK {
+			ftc.progress.SetHeadlineMsg("Ready for your DevOps journey? Please hang on while we creat your environment")
+			statusResponse := myJfrogGetStatusResponse{}
+			if err = json.Unmarshal(body, &statusResponse); err != nil {
+				return true, nil, err
+			}
+			// Got the new server deatiles
+			if statusResponse.Ready {
+				return true, body, nil
+			}
+		}
+		return false, nil, nil
+	}
+
+	pollingExecutor := &httputils.PollingExecutor{
+		Timeout:         maxWaitMinutes,
+		PollingInterval: syncSleepInterval,
+		PollingAction:   pollingAction,
+	}
+
+	body, err := pollingExecutor.Execute()
+	if err != nil {
+		return nil, err
+	}
+	statusResponse := myJfrogGetStatusResponse{}
+	if err = json.Unmarshal(body, &statusResponse); err != nil {
+		return nil, errorutils.CheckError(err)
+	}
+	ftc.progress.ClearHeadlineMsg()
+	serverDetails = &config.ServerDetails{
+		Url:         statusResponse.PlatformUrl,
+		AccessToken: statusResponse.AccessToken,
+	}
+	ftc.serverDetails = serverDetails
+	return serverDetails, nil
+}
+
+// Add the given server details to the cli's config by running a 'jf config' command
+func configServer(server *config.ServerDetails) error {
+	u, err := url.Parse(server.Url)
+	if errorutils.CheckError(err) != nil {
+		return err
+	}
+	// Take the server name from host name: https://myjfrog.jfrog.com/ -> myjfrog
+	serverId := strings.Split(u.Host, ".")[0]
+	configCmd := commands.NewConfigCommand().SetInteractive(false).SetServerId(serverId).SetDetails(server)
+	return configCmd.Config()
+}
+
+type myJfrogGetStatusRequest struct {
+	CliRegistrationId string `json:"cliRegistrationId,omitempty"`
+}
+
+type myJfrogGetStatusResponse struct {
+	CliRegistrationId string `json:"cliRegistrationId,omitempty"`
+	Ready             bool   `json:"ready,omitempty"`
+	AccessToken       string `json:"accessToken,omitempty"`
+	PlatformUrl       string `json:"platformUrl,omitempty"`
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/c-bata/go-prompt v0.2.5
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e
 	github.com/codegangsta/cli v1.20.0
+	github.com/google/uuid v1.3.0
 	github.com/gookit/color v1.4.2
 	github.com/jedib0t/go-pretty/v6 v6.2.2
 	github.com/jfrog/build-info-go v0.1.2
@@ -16,6 +17,7 @@ require (
 	github.com/magiconair/properties v1.8.5
 	github.com/manifoldco/promptui v0.8.0
 	github.com/mattn/go-shellwords v1.0.3
+	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/viper v1.8.1
 	github.com/stretchr/testify v1.7.0
@@ -30,7 +32,7 @@ exclude (
 	github.com/pkg/sftp v1.10.1
 )
 
-replace github.com/jfrog/jfrog-client-go => github.com/jfrog/jfrog-client-go v1.6.3-0.20211125104048-cb20703ca609
+replace github.com/jfrog/jfrog-client-go => github.com/jfrog/jfrog-client-go v1.6.3-0.20211129155531-7d566d06876a
 
 replace github.com/jfrog/gocmd => github.com/jfrog/gocmd v0.5.6-0.20211125095246-93273e52cec4
 

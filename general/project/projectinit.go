376f21190e4711d1840ed2a4290bfc0389a6db13
eyalbe4 <eyalbenmoshe@jfrog.com>
2021-12-02 16:18:06 +0200

    Add message to the 'project init' command

--- general/project/projectinit.go
+++ general/project/projectinit.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/gookit/color"
 	artifactoryCommandsUtils "github.com/jfrog/jfrog-cli-core/v2/artifactory/commands/utils"
 	artifactoryUtils "github.com/jfrog/jfrog-cli-core/v2/artifactory/utils"
 	"github.com/jfrog/jfrog-cli-core/v2/utils/config"
@@ -46,7 +47,7 @@ func (pic *ProjectInitCommand) Run() (err error) {
 		}
 		pic.serverId = defaultServer.ServerId
 	}
-	technologiesMap, err := coreutils.DetectTechnologies(pic.projectPath, false, true)
+	technologiesMap, err := coreutils.DetectTechnologies(pic.projectPath, false, false)
 	if err != nil {
 		return err
 	}
@@ -65,18 +66,94 @@ func (pic *ProjectInitCommand) Run() (err error) {
 		}
 	}
 	// Create build config
-	err = pic.createBuildConfig()
-	if err == nil {
-		message :=
-			"You're all set!\n" +
-				"The project config is stored inside the .jfrog directory.\n" +
-				"Here's how you get started -\n" +
-				coreutils.GettingStartedGuideUrl
-		fmt.Println()
-		err = coreutils.PrintTable("", "", message)
-		fmt.Println()
+	if err = pic.createBuildConfig(); err != nil {
+		return
 	}
+
+	message :=
+		bold("You're all set!") +
+			"\n\n" +
+			bold("The project config is stored inside the .jfrog directory.") +
+			"\n\n" +
+			title("Audit your code project for security vulnerabilities by running") +
+			"\n" +
+			"jf audit\n\n" +
+			title("Scan any software package on this machine for security vulnerabilities by running") +
+			"\n" +
+			"jf scan path/to/dir/or/package\n\n" +
+			pic.createBuildMessage(technologiesMap) +
+			title("Read more using this link:") +
+			"\n" +
+			link(coreutils.GettingStartedGuideUrl)
+	fmt.Println()
+	err = coreutils.PrintTable("", "", message)
+	fmt.Println()
+
 	return
+}
+
+// Return a string message, which includes all the build and deployment commands, matching the technologiesMap sent.
+func (pic *ProjectInitCommand) createBuildMessage(technologiesMap map[coreutils.Technology]bool) string {
+	message := ""
+	for tech, detected := range technologiesMap {
+		if detected {
+			switch tech {
+			case coreutils.Maven:
+				message += "jf mvn install deploy\n"
+			case coreutils.Gradle:
+				message += "jf gradle artifactoryP\n"
+			case coreutils.Npm:
+				message += "jf npm install publish\n"
+			case coreutils.Go:
+				message +=
+					"jf go build\n" +
+						"jf go-publish v1.0.0\n"
+			case coreutils.Pypi:
+				message +=
+					"jf pip install\n" +
+						"jf rt u path/to/package/file default-pypi-local" + comment(" # Publish your pip package") +
+						"\n"
+			}
+		}
+	}
+	if message != "" {
+		message = title("Build the code & deploy the packages by running") +
+			"\n" +
+			message +
+			"\n" +
+			title("Publish the build-info to Artifactory") +
+			"\n" +
+			"jf rt bp\n\n"
+	}
+	return message
+}
+
+// Print the test to the console in green color.
+func title(str string) string {
+	return colorStr(str, color.Green)
+}
+
+// Print the test to the console in cyan color.
+func link(str string) string {
+	return colorStr(str, color.Cyan)
+}
+
+// Print the test to the console with bold style.
+func bold(str string) string {
+	return colorStr(str, color.Bold)
+}
+
+// Print the test to the console in gray color.
+func comment(str string) string {
+	return colorStr(str, color.Gray)
+}
+
+// Print the test to the console with the specified color.
+func colorStr(str string, c color.Color) string {
+	if coreutils.IsTerminal() {
+		return c.Render(str)
+	}
+	return str
 }
 
 type BuildConfigFile struct {

7ad5d4368a16517ebb3cbec65ffdb7b4584bb413
Michael Sverdlov <sverdlov93@gmail.com>
2022-01-24 15:28:17 +0200

    Update downloadindexer.go

--- xray/utils/downloadindexer.go
+++ xray/utils/downloadindexer.go
@@ -53,7 +53,7 @@ func DownloadIndexerIfNeeded(xrayManager *xray.XrayServicesManager, xrayVersionS
 		return
 	}
 
-	log.Info("JFrog Xray Indexer is not cached locally. Downloading it now...")
+	log.Info("JFrog Xray Indexer " + xrayVersionStr + " is not cached locally. Downloading it now...")
 	indexerPath, err = downloadIndexer(xrayManager, indexerDirPath, indexerBinaryName)
 	return
 }
@@ -108,6 +108,7 @@ func downloadIndexer(xrayManager *xray.XrayServicesManager, indexerDirPath, inde
 	if err != nil {
 		return "", err
 	}
+	log.Info("The downloaded Xray Indexer version is " + indexerVersion)
 	newDirPath := filepath.Join(indexerDirPath, indexerVersion)
 
 	// In case of a hot upgrade of Xray in progress, the version of the downloaded indexer might be different from the Xray version we got above,

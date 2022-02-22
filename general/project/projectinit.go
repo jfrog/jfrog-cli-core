cc2656797ec001dbfa9cddf167b1d566ea61746e
Or Geva <orgab@jfrog.com>
2022-02-22 16:43:27 +0200

    Add missing :<tag>

--- general/project/projectinit.go
+++ general/project/projectinit.go
@@ -139,7 +139,7 @@ func (pic *ProjectInitCommand) createBuildMessage(technologiesMap map[coreutils.
 		// docker exists in path.
 		message += "\n" + coreutils.PrintTitle("Pull and Push any docker image using Artifactory") +
 			"\n" +
-			"1. jf docker tag <image> my.artifactory.url/<image>:<tag>\n" +
+			"1. jf docker tag <image>:<tag> my.artifactory.url/<image>:<tag>\n" +
 			"2. jf docker pull my.artifactory.url/<image>:<tag>\n" +
 			"3. jf docker push my.artifactory.url/<image>:<tag>\n"
 	}

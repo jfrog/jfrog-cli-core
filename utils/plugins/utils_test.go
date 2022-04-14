d1e29190993edfbde456e2a947ea9ffdf65fb7c7
Gai Lazar <gail@jfrog.com>
2022-04-14 20:08:56 +0300

    Add optional 'resources' directory to plugins (#374)
    *Adding an option to create and publish plugins to Artifactory with a directory named "resources", which contains all resources that the developer wants to add to the plugin.
    *When publishing a plugin make sure that you have a "resources" directory in root.
    *Resources are being uploaded to Artifactory in a zip file.
    *There are changes in the plugins directory hierarchy at the local '.jfrog' directory.

---
+++ utils/plugins/utils_test.go
@@ -0,0 +1,87 @@
+package plugins
+
+//const pluginName = "rt-fs"
+
+//func TestConvertPluginsV0ToV1(t *testing.T) {
+//	// Setup testing env
+//	cleanUpTempEnv := createTempEnvForPluginsTests(t)
+//	defer cleanUpTempEnv()
+//	testHomeDir := setupPluginsTestingEnv(t, "v0")
+//	// Migration- v0 to v1
+//	p, err := convertPluginsV0ToV1()
+//	assert.NoError(t, err)
+//	assert.Equal(t, p.Version, 1)
+//	// Verity plugins.yaml exists
+//	expectedYamlLocation := filepath.Join(testHomeDir, coreutils.JfrogPluginsDirName, coreutils.JfrogPluginsFileName)
+//	exists, err := fileutils.IsFileExists(expectedYamlLocation, false)
+//	assert.NoError(t, err)
+//	assert.True(t, exists, fmt.Sprintf("expected file: %s doesn't exists", expectedYamlLocation))
+//	// Verify executable location according to v1 hierarchy
+//	exists, err = fileutils.IsFileExists(filepath.Join(testHomeDir, coreutils.JfrogPluginsDirName, pluginName, coreutils.PluginsExecDirName, GetLocalPluginExecutableName(pluginName)), false)
+//	assert.NoError(t, err)
+//	assert.True(t, exists)
+//	assert.NoError(t, os.RemoveAll(filepath.Join(testHomeDir, coreutils.JfrogPluginsDirName)))
+//}
+//
+//// Plugins directory is empty - only 'plugins.yaml' should be created.
+//func TestConvertPluginsV0ToV1EmptyDir(t *testing.T) {
+//	// Setup testing env
+//	cleanUpTempEnv := createTempEnvForPluginsTests(t)
+//	defer cleanUpTempEnv()
+//	testHomeDir := setupPluginsTestingEnv(t, "empty")
+//	// Migration- v0 to v1
+//	p, err := convertPluginsV0ToV1()
+//	assert.NoError(t, err)
+//	assert.Equal(t, p.Version, 1)
+//	// Verity plugins.yaml exists
+//	expectedYamlLocation := filepath.Join(testHomeDir, coreutils.JfrogPluginsDirName, coreutils.JfrogPluginsFileName)
+//	exists, err := fileutils.IsFileExists(expectedYamlLocation, false)
+//	assert.NoError(t, err)
+//	assert.True(t, exists, fmt.Sprintf("expected file: %s doesn't exists", expectedYamlLocation))
+//	assert.NoError(t, os.RemoveAll(filepath.Join(testHomeDir, coreutils.JfrogPluginsDirName)))
+//}
+//
+//// Plugins directory contains unexpected file (non executable)
+//func TestConvertPluginsV0ToV1WithUnexpectedFiles(t *testing.T) {
+//	// Setup testing env
+//	cleanUpTempEnv := createTempEnvForPluginsTests(t)
+//	defer cleanUpTempEnv()
+//	testHomeDir := setupPluginsTestingEnv(t, "unexpectedFiles")
+//	// Migration- v0 to v1
+//	p, err := convertPluginsV0ToV1()
+//	assert.NoError(t, err)
+//	assert.Equal(t, p.Version, 1)
+//	// Verity plugins.yaml exists
+//	expectedYamlLocation := filepath.Join(testHomeDir, coreutils.JfrogPluginsDirName, coreutils.JfrogPluginsFileName)
+//	exists, err := fileutils.IsFileExists(expectedYamlLocation, false)
+//	assert.NoError(t, err)
+//	assert.True(t, exists, fmt.Sprintf("expected file: %s doesn't exists", expectedYamlLocation))
+//	// Verify executable location according to v1 hierarchy
+//	exists, err = fileutils.IsFileExists(filepath.Join(testHomeDir, coreutils.JfrogPluginsDirName, pluginName, coreutils.PluginsExecDirName, GetLocalPluginExecutableName(pluginName)), false)
+//	assert.NoError(t, err)
+//	assert.True(t, exists)
+//	assert.NoError(t, os.RemoveAll(filepath.Join(testHomeDir, coreutils.JfrogPluginsDirName)))
+//}
+//
+//func setupPluginsTestingEnv(t *testing.T, pluginsDirName string) string {
+//	testHomeDir, err := coreutils.GetJfrogHomeDir()
+//	assert.NoError(t, err)
+//	wd, err := os.Getwd()
+//	assert.NoError(t, err)
+//	err = fileutils.CopyDir(filepath.Join(wd, "testdata", coreutils.JfrogPluginsDirName, pluginsDirName), filepath.Join(testHomeDir, coreutils.JfrogPluginsDirName), true, nil)
+//	assert.NoError(t, err)
+//	coreutils.ChmodPluginsDirectoryContent()
+//	return testHomeDir
+//}
+//
+//// Set JFROG_CLI_HOME_DIR environment variable to be a new temp directory
+//func createTempEnvForPluginsTests(t *testing.T) (cleanUp func()) {
+//	tmpDir, err := ioutil.TempDir("", "plugins_test")
+//	assert.NoError(t, err)
+//	oldHome := os.Getenv(coreutils.HomeDir)
+//	testsutils.SetEnvAndAssert(t, coreutils.HomeDir, tmpDir)
+//	return func() {
+//		testsutils.RemoveAllAndAssert(t, tmpDir)
+//		testsutils.SetEnvAndAssert(t, coreutils.HomeDir, oldHome)
+//	}
+//}

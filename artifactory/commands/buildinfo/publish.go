09f0a85a1953e166f1d24c636664c9bc2395db3a
Robi Nino <robi.nino@gmail.com>
2022-04-20 10:17:19 +0300

    dont output in detailed summary

--- artifactory/commands/buildinfo/publish.go
+++ artifactory/commands/buildinfo/publish.go
@@ -129,21 +129,28 @@ func (bpc *BuildPublishCommand) Run() error {
 		return err
 	}
 
-	log.Info("Build info successfully deployed.")
-	err = bpc.logJsonOutput(servicesManager, buildInfo.Started)
+	buildLink, err := bpc.constructBuildInfoUiUrl(servicesManager, buildInfo.Started)
 	if err != nil {
 		return err
 	}
-	return build.Clean()
+
+	err = build.Clean()
+	if err != nil {
+		return err
+	}
+
+	logMsg := "Build info successfully deployed."
+	if bpc.IsDetailedSummary() {
+		log.Info(logMsg + " Browse it in Artifactory under " + buildLink)
+		return nil
+	}
+
+	log.Info(logMsg)
+	return logJsonOutput(buildLink)
 }
 
-func (bpc *BuildPublishCommand) logJsonOutput(servicesManager artifactory.ArtifactoryServicesManager, buildInfoStarted string) error {
-	buildLink, err := bpc.constructBuildInfoUiUrl(servicesManager, buildInfoStarted)
-	if err != nil {
-		return err
-	}
-
-	output := formats.BuildPublishOutput{BuildInfoUiUrl: buildLink}
+func logJsonOutput(buildInfoUiUrl string) error {
+	output := formats.BuildPublishOutput{BuildInfoUiUrl: buildInfoUiUrl}
 	results, err := json.Marshal(output)
 	if err != nil {
 		return errorutils.CheckError(err)

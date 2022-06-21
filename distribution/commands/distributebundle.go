04c2d33b841b4a66624297b4c29b688b9224f61b
sverdlov93 <sverdlov93@gmail.com>
2022-06-21 18:09:41 +0300

    add createRepo flag

--- distribution/commands/distributebundle.go
+++ distribution/commands/distributebundle.go
@@ -14,6 +14,7 @@ type DistributeReleaseBundleCommand struct {
 	sync                    bool
 	maxWaitMinutes          int
 	dryRun                  bool
+	createRepo              bool
 }
 
 func NewReleaseBundleDistributeCommand() *DistributeReleaseBundleCommand {
@@ -50,6 +51,11 @@ func (db *DistributeReleaseBundleCommand) SetDryRun(dryRun bool) *DistributeRele
 	return db
 }
 
+func (db *DistributeReleaseBundleCommand) SetCreateRepo(createRepo bool) *DistributeReleaseBundleCommand {
+	db.createRepo = createRepo
+	return db
+}
+
 func (db *DistributeReleaseBundleCommand) Run() error {
 	servicesManager, err := utils.CreateDistributionServiceManager(db.serverDetails, db.dryRun)
 	if err != nil {
@@ -61,9 +67,9 @@ func (db *DistributeReleaseBundleCommand) Run() error {
 	}
 
 	if db.sync {
-		return servicesManager.DistributeReleaseBundleSync(db.distributeBundlesParams, db.maxWaitMinutes)
+		return servicesManager.DistributeReleaseBundleSync(db.distributeBundlesParams, db.maxWaitMinutes, db.createRepo)
 	}
-	return servicesManager.DistributeReleaseBundle(db.distributeBundlesParams)
+	return servicesManager.DistributeReleaseBundle(db.distributeBundlesParams, db.createRepo)
 }
 
 func (db *DistributeReleaseBundleCommand) ServerDetails() (*config.ServerDetails, error) {

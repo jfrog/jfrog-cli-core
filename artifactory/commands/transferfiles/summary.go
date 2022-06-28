554ac6f3a032e61d5cd36a57500004ad608c78af
sverdlov93 <sverdlov93@gmail.com>
2022-06-28 15:52:03 +0300

    add summary reader
    Signed-off-by: sverdlov93 <sverdlov93@gmail.com>

--- artifactory/commands/transferfiles/summary.go
+++ artifactory/commands/transferfiles/summary.go
@@ -2,10 +2,13 @@ package transferfiles
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gocarina/gocsv"
 	"github.com/jfrog/jfrog-client-go/utils/errorutils"
 	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
 	"os"
+	"strconv"
+	"time"
 )
 
 func GenerateSummaryFiles(logPaths []string) (err error) {
@@ -13,7 +16,8 @@ func GenerateSummaryFiles(logPaths []string) (err error) {
 	if err != nil {
 		return
 	}
-	summaryCsv, err := os.Create("mySummary.csv")
+	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+	summaryCsv, err := os.Create(fmt.Sprintf("logs-%s.csv", timestamp))
 	if err != nil {
 		return
 	}
@@ -33,19 +37,21 @@ func ReadErrorsFromLogFiles(logPaths []string) (allErrors FilesErrors, err error
 		if err != nil {
 			return
 		}
-		if exists {
-			var content []byte
-			content, err = fileutils.ReadFile(logPath)
-			if err != nil {
-				return
-			}
-			fileErrors := new(FilesErrors)
-			err = errorutils.CheckError(json.Unmarshal(content, &fileErrors))
-			if err != nil {
-				return
-			}
-			allErrors.Errors = append(allErrors.Errors, fileErrors.Errors...)
+		if !exists {
+			err = fmt.Errorf("log file: %s does not exist.", logPath)
+			return
 		}
+		var content []byte
+		content, err = fileutils.ReadFile(logPath)
+		if err != nil {
+			return
+		}
+		fileErrors := new(FilesErrors)
+		err = errorutils.CheckError(json.Unmarshal(content, &fileErrors))
+		if err != nil {
+			return
+		}
+		allErrors.Errors = append(allErrors.Errors, fileErrors.Errors...)
 	}
 	return
 }

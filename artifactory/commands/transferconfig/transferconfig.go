e8379b20c1a4e8ac95fa26d0c6d2a25f89b56502
Asaf Gabai <asafg@jfrog.com>
2022-07-12 16:04:20 +0300

    Improved log in transfer-config command when the target repository is not empty.

--- artifactory/commands/transferconfig/transferconfig.go
+++ artifactory/commands/transferconfig/transferconfig.go
@@ -178,7 +178,7 @@ func (tcc *TransferConfigCommand) validateArtifactoryServers(targetServicesManag
 	}
 	// We consider an "empty" Artifactory as an Artifactory server that contains 2 users: the admin user and the anonymous.
 	if len(users) > 2 {
-		return errorutils.CheckErrorf("cowardly refusing to import the config to the target server, because it contains more than 2 users. You can bypass this rule by providing the --force flag.")
+		return errorutils.CheckErrorf("cowardly refusing to import the config to the target server, because it contains more than 2 users. By default, this command avoids transferring the config to a server which isn't empty. You can bypass this rule by providing the --force flag to the transfer-config command.")
 	}
 	return nil
 }

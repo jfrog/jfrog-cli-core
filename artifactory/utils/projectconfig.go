a8d669bf2a616c87c22e6041a520a83dbfa69f28
Michael Sverdlov <sverdlov93@gmail.com>
2022-08-28 11:13:25 +0300

    Fix GetProjectConfFilePath return value (#521)

--- artifactory/utils/projectconfig.go
+++ artifactory/utils/projectconfig.go
@@ -80,26 +80,30 @@ func GetProjectConfFilePath(projectType ProjectType) (confFilePath string, exist
 	confFileName := filepath.Join("projects", projectType.String()+".yaml")
 	projectDir, exists, err := fileutils.FindUpstream(".jfrog", fileutils.Dir)
 	if err != nil {
-		return "", false, err
+		return
 	}
 	if exists {
-		confFilePath = filepath.Join(projectDir, ".jfrog", confFileName)
-		exists, err = fileutils.IsFileExists(confFilePath, false)
+		filePath := filepath.Join(projectDir, ".jfrog", confFileName)
+		exists, err = fileutils.IsFileExists(filePath, false)
 		if err != nil {
-			return "", false, err
+			return
 		}
 
 		if exists {
+			confFilePath = filePath
 			return
 		}
 	}
 	// If missing in the root project, check in the home dir
 	jfrogHomeDir, err := coreutils.GetJfrogHomeDir()
 	if err != nil {
-		return "", exists, err
+		return
 	}
-	confFilePath = filepath.Join(jfrogHomeDir, confFileName)
-	exists, err = fileutils.IsFileExists(confFilePath, false)
+	filePath := filepath.Join(jfrogHomeDir, confFileName)
+	exists, err = fileutils.IsFileExists(filePath, false)
+	if exists {
+		confFilePath = filePath
+	}
 	return
 }
 

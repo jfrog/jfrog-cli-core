2a38a400cdad16a471bb61730c9976555151a393
Omer Zidkoni <50792403+omerzi@users.noreply.github.com>
2022-09-11 13:28:54 +0300

    Improvement - fix technology naming (#540)

--- xray/formats/simplejsonapi.go
+++ xray/formats/simplejsonapi.go
@@ -1,5 +1,7 @@
 package formats
 
+import "github.com/jfrog/jfrog-cli-core/v2/utils/coreutils"
+
 // Structs in this file should NOT be changed!
 // The structs are used as an API for the simple-json format, thus changing their structure or the 'json' annotation will break the API.
 
@@ -28,7 +30,7 @@ type VulnerabilityOrViolationRow struct {
 	References               []string                  `json:"references"`
 	ImpactPaths              [][]ComponentRow          `json:"impactPaths"`
 	JfrogResearchInformation *JfrogResearchInformation `json:"jfrogResearchInformation"`
-	Technology               string                    `json:"-"`
+	Technology               coreutils.Technology      `json:"-"`
 }
 
 type LicenseRow struct {

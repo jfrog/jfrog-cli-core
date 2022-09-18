b573f2f78243137758bf5c8e4c00fc0409713f1f
Michael Sverdlov <sverdlov93@gmail.com>
2022-09-18 18:03:30 +0300

    Take pip3 instead of pip on pip audit command (#556)

--- xray/audit/python/python.go
+++ xray/audit/python/python.go
@@ -106,6 +106,7 @@ func getDependencies(pythonTool pythonutils.PythonTool, requirementsFile string)
 }
 
 func runPythonInstall(pythonTool pythonutils.PythonTool, requirementsFile string) (restoreEnv func() error, err error) {
+	var output []byte
 	switch pythonTool {
 	case pythonutils.Pip:
 		restoreEnv, err = SetPipVirtualEnvPath()
@@ -113,23 +114,24 @@ func runPythonInstall(pythonTool pythonutils.PythonTool, requirementsFile string
 			return
 		}
 		// Run pip install
-		var output []byte
+		pipExec, _ := exec.LookPath("pip3")
+		if pipExec == "" {
+			pipExec = "pip"
+		}
 		if requirementsFile != "" {
 			clientLog.Debug("Running pip install -r", requirementsFile)
-			output, err = exec.Command("pip", "install", "-r", requirementsFile).CombinedOutput()
+			output, err = exec.Command(pipExec, "install", "-r", requirementsFile).CombinedOutput()
 		} else {
 			clientLog.Debug("Running 'pip install .'")
-			output, err = exec.Command("pip", "install", ".").CombinedOutput()
+			output, err = exec.Command(pipExec, "install", ".").CombinedOutput()
 			if err != nil {
 				err = errorutils.CheckErrorf("pip install command failed: %s - %s", err.Error(), output)
 				clientLog.Debug(fmt.Sprintf("Failed running 'pip install .' : \n%s\n trying 'pip install -r requirements.txt' ", err.Error()))
 				// Run pip install -r requirements
-				output, err = exec.Command("pip", "install", "-r", "requirements.txt").CombinedOutput()
+				output, err = exec.Command(pipExec, "install", "-r", "requirements.txt").CombinedOutput()
 			}
 		}
-		if err != nil {
-			err = errorutils.CheckErrorf("pip install command failed: %s - %s", err.Error(), output)
-		}
+
 	case pythonutils.Pipenv:
 		// Set virtualenv path to venv dir
 		err = os.Setenv("WORKON_HOME", ".jfrog")
@@ -140,11 +142,7 @@ func runPythonInstall(pythonTool pythonutils.PythonTool, requirementsFile string
 			return os.Unsetenv("WORKON_HOME")
 		}
 		// Run pipenv install
-		var output []byte
 		output, err = exec.Command("pipenv", "install", "-d").CombinedOutput()
-		if err != nil {
-			err = errorutils.CheckErrorf("pipenv install command failed: %s - %s", err.Error(), output)
-		}
 
 	case pythonutils.Poetry:
 		// No changes to env here.
@@ -152,12 +150,11 @@ func runPythonInstall(pythonTool pythonutils.PythonTool, requirementsFile string
 			return nil
 		}
 		// Run poetry install
-		var output []byte
 		output, err = exec.Command("poetry", "install").CombinedOutput()
-		if err != nil {
-			err = errorutils.CheckErrorf("poetry install command failed: %s - %s", err.Error(), output)
-			return
-		}
+	}
+
+	if err != nil {
+		err = errorutils.CheckErrorf("%s install command failed: %s - %s", string(pythonTool), err.Error(), output)
 	}
 	return
 }

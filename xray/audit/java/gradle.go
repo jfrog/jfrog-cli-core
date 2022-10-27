1deb068bfc053a35a6f4ebafcf2a5d536cc2176e
Omer Zidkoni <omerz@jfrog.com>
2022-10-27 10:00:15 +0300

    verify gradle wrapper exists

--- xray/audit/java/gradle.go
+++ xray/audit/java/gradle.go
@@ -2,11 +2,19 @@ package java
 
 import (
 	"fmt"
+	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
+	"os"
+	"path/filepath"
 
 	"github.com/jfrog/jfrog-cli-core/v2/artifactory/utils"
 	gradleutils "github.com/jfrog/jfrog-cli-core/v2/utils/gradle"
 	"github.com/jfrog/jfrog-client-go/utils/log"
 	"github.com/jfrog/jfrog-client-go/xray/services"
+)
+
+const (
+	gradlew    = "gradlew"
+	gradlewbat = "gradlew.bat"
 )
 
 func BuildGradleDependencyTree(excludeTestDeps, useWrapper, ignoreConfigFile bool) (dependencyTree []*services.GraphNode, err error) {
@@ -40,10 +48,36 @@ func runGradle(buildConfiguration *utils.BuildConfiguration, excludeTestDeps, us
 			log.Debug("Using resolver config from", configFilePath)
 		}
 	}
+	// check if gradle wrapper exist
+	if useWrapper {
+		var wrapperExist bool
+		wrapperExist, err = verifyGradleWrapper()
+		if err != nil {
+			return
+		}
+		useWrapper = wrapperExist
+	}
 	// Read config
 	vConfig, err := utils.ReadGradleConfig(configFilePath, useWrapper)
 	if err != nil {
 		return err
 	}
 	return gradleutils.RunGradle(vConfig, tasks, "", buildConfiguration, 0, useWrapper, true)
+}
+
+func verifyGradleWrapper() (bool, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return false, err
+	}
+	filesInDir, err := fileutils.ListFiles(wd, false)
+	fullPathGradlew, err := filepath.Abs(gradlew)
+	fullPathGradlewbat, err := filepath.Abs(gradlewbat)
+	for _, file := range filesInDir {
+		if file == fullPathGradlew || file == fullPathGradlewbat {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }

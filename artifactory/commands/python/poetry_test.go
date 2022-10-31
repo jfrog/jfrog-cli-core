85379770068fc8fa4a90605ec07e0421809731c9
Tal Arian <tala@jfrog.com>
2022-10-31 18:28:43 +0200

    Add tests for Poetry repo config (#606)

---
+++ artifactory/commands/python/poetry_test.go
@@ -0,0 +1,55 @@
+package python
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/jfrog/jfrog-cli-core/v2/xray/audit"
+	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddRepoToPyprojectFile(t *testing.T) {
+	poetryProjectPath, cleanUp := initPoetryTest(t)
+	defer cleanUp()
+	pyProjectPath := filepath.Join(poetryProjectPath, "pyproject.toml")
+	dummyRepoName := "test-repo-name"
+	dummyRepoURL := "https://ecosysjfrog.jfrog.io/"
+
+	err := addRepoToPyprojectFile(pyProjectPath, dummyRepoName, dummyRepoURL)
+	assert.NoError(t, err)
+	// Validate pyproject.toml file content
+	content, err := fileutils.ReadFile(pyProjectPath)
+	assert.NoError(t, err)
+	assert.Contains(t, string(content), dummyRepoURL)
+}
+
+func TestPoetryCommandCleanup(t *testing.T) {
+	poetryProjectPath, cleanUp := initPoetryTest(t)
+	defer cleanUp()
+
+	pc := NewPoetryCommand()
+	dummyRepoURL := "https://ecosysjfrog.jfrog.io/"
+	err := pc.configPoetryRepo(dummyRepoURL, "", "")
+	assert.NoError(t, err)
+
+	pyProjectPath := filepath.Join(poetryProjectPath, "pyproject.toml")
+	content, err := fileutils.ReadFile(pyProjectPath)
+	assert.NoError(t, err)
+	assert.Contains(t, string(content), dummyRepoURL)
+
+	pc.cleanup()
+
+	content, err = fileutils.ReadFile(pyProjectPath)
+	assert.NoError(t, err)
+	assert.NotContains(t, string(content), dummyRepoURL)
+}
+
+func initPoetryTest(t *testing.T) (string, func()) {
+	// Create and change directory to test workspace
+	testAbs, err := filepath.Abs(filepath.Join("..", "..", "..", "xray", "commands", "testdata", "poetry-project"))
+	assert.NoError(t, err)
+	poetryProjectPath, cleanUp := audit.CreateTestWorkspace(t, "poetry-project")
+	assert.NoError(t, fileutils.CopyDir(testAbs, poetryProjectPath, true, nil))
+	return poetryProjectPath, cleanUp
+}

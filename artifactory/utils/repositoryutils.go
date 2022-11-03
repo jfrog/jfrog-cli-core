9122af6426455256ba779df1b3667428c849561b
yahavi <yahavi@jfrog.com>
2022-11-03 13:44:08 +0200

    Transfer - Add blacklist for repositories

--- artifactory/utils/repositoryutils.go
+++ artifactory/utils/repositoryutils.go
@@ -34,6 +34,10 @@ func (repoType RepoType) String() string {
 	return RepoTypes[repoType]
 }
 
+var blacklistedRepositories = []string{
+	"jfrog-usage-logs", "jfrog-billing-logs", "jfrog-logs", "artifactory-pipe-info", "auto-trashcan", "jfrog-support-bundle", "_intransit", "artifactory-edge-uploads",
+}
+
 // GetRepositories returns the names of local, remote, virtual or federated repositories filtered by their type.
 // artDetails - Artifactory server details
 // repoTypes - Repository types to filter. If empty - return all repository types.
@@ -184,7 +188,7 @@ func (rf *RepositoryFilter) ShouldIncludeRepository(repoKey string) (bool, error
 	}
 
 	// Check if this repository name matches any exclude pattern.
-	for _, excludePattern := range rf.ExcludePatterns {
+	for _, excludePattern := range append(rf.ExcludePatterns, blacklistedRepositories...) {
 		matched, err := path.Match(excludePattern, repoKey)
 		if err != nil {
 			return false, err

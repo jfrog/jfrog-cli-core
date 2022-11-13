46caadba4a4e89f1ed74347221282c57af36437a
Michael Sverdlov <sverdlov93@gmail.com>
2022-11-13 14:34:41 +0200

    Improve interactive 'config add' UI (#617)

--- common/commands/config.go
+++ common/commands/config.go
@@ -279,7 +279,7 @@ func (cc *ConfigCommand) getConfigurationFromUser() error {
 	disallowUsingSavedPassword := false
 
 	if cc.details.Url == "" {
-		ioutils.ScanFromConsole("JFrog platform URL", &cc.details.Url, cc.defaultDetails.Url)
+		ioutils.ScanFromConsole("JFrog Platform URL", &cc.details.Url, cc.defaultDetails.Url)
 	}
 
 	if fileutils.IsSshUrl(cc.details.Url) {
@@ -510,7 +510,7 @@ func printConfigs(configuration []*config.ServerDetails) {
 	for _, details := range configuration {
 		isDefault := details.IsDefault
 		logIfNotEmpty(details.ServerId, "Server ID:\t\t\t", false, isDefault)
-		logIfNotEmpty(details.Url, "JFrog platform URL:\t\t", false, isDefault)
+		logIfNotEmpty(details.Url, "JFrog Platform URL:\t\t", false, isDefault)
 		logIfNotEmpty(details.ArtifactoryUrl, "Artifactory URL:\t\t", false, isDefault)
 		logIfNotEmpty(details.DistributionUrl, "Distribution URL:\t\t", false, isDefault)
 		logIfNotEmpty(details.XrayUrl, "Xray URL:\t\t\t", false, isDefault)

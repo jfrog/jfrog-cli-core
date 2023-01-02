5f1ed368dc6b3446a1cd06120906f0fd98bfaa10
Michael Sverdlov <michaelsv@jfrog.com>
2023-01-02 15:52:06 +0200

    fix slashes

--- utils/coreutils/utils.go
+++ utils/coreutils/utils.go
@@ -20,8 +20,15 @@ import (
 
 const (
 	GettingStartedGuideUrl = "https://github.com/jfrog/jfrog-cli/blob/v2/guides/getting-started-with-jfrog-using-the-cli.md"
-	Artifactory            = "Artifactory"
-	Xray                   = "Xray"
+)
+
+type MinVersionProduct string
+
+const (
+	Artifactory  MinVersionProduct = "Artifactory"
+	Xray         MinVersionProduct = "Xray"
+	DataTransfer MinVersionProduct = "Data Transfer"
+	DockerApi    MinVersionProduct = "Docker API"
 )
 
 // Error modes (how should the application behave when the CheckError function is invoked):
@@ -499,11 +506,11 @@ func GetJfrogTransferDir() (string, error) {
 	return filepath.Join(homeDir, JfrogTransferDirName), nil
 }
 
-func ValidateMinimumVersion(endPoint, currentVersion, minimumVersion string) error {
+func ValidateMinimumVersion(product MinVersionProduct, currentVersion, minimumVersion string) error {
 	if !version.NewVersion(currentVersion).AtLeast(minimumVersion) {
 		return errorutils.CheckErrorf(fmt.Sprintf("You are using %s version %s,"+
 			" while this operation requires version %s or higher.",
-			endPoint, currentVersion, minimumVersion))
+			product, currentVersion, minimumVersion))
 	}
 	return nil
 }

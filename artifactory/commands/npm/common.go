f605be2ff96180757d36c606bf09f6db51581818
Omer Zidkoni <50792403+omerzi@users.noreply.github.com>
2023-02-27 11:57:30 +0200

    Fix node v18+ .nprmc private registry E401 issue (#695)

--- artifactory/commands/npm/common.go
+++ artifactory/commands/npm/common.go
@@ -2,6 +2,7 @@ package npm
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -18,7 +19,9 @@ import (
 )
 
 const (
-	npmConfigAuthEnv = "NPM_CONFIG__AUTH"
+	npmConfigAuthEnv       = "npm_config_%s:_auth"
+	npmVersionForLegacyEnv = "9.3.1"
+	npmLegacyConfigAuthEnv = "npm_config__auth"
 )
 
 type CommonArgs struct {
@@ -37,130 +40,155 @@ type CommonArgs struct {
 	NpmCommand
 }
 
-func (com *CommonArgs) preparePrerequisites(repo string, overrideNpmrc bool) error {
+func (ca *CommonArgs) preparePrerequisites(repo string, overrideNpmrc bool) error {
 	log.Debug("Preparing prerequisites...")
 	var err error
-	com.npmVersion, com.executablePath, err = biutils.GetNpmVersionAndExecPath(log.Logger)
+	ca.npmVersion, ca.executablePath, err = biutils.GetNpmVersionAndExecPath(log.Logger)
 	if err != nil {
 		return err
 	}
 	if !overrideNpmrc {
 		return nil
 	}
-	if com.npmVersion.Compare(minSupportedNpmVersion) > 0 {
+	if ca.npmVersion.Compare(minSupportedNpmVersion) > 0 {
 		return errorutils.CheckErrorf(
-			"JFrog CLI npm %s command requires npm client version "+minSupportedNpmVersion+" or higher. The Current version is: %s", com.cmdName, com.npmVersion.GetVersion())
+			"JFrog CLI npm %s command requires npm client version "+minSupportedNpmVersion+" or higher. The Current version is: %s", ca.cmdName, ca.npmVersion.GetVersion())
 	}
 
-	if err := com.setJsonOutput(); err != nil {
+	if err := ca.setJsonOutput(); err != nil {
 		return err
 	}
 
-	com.workingDirectory, err = coreutils.GetWorkingDirectory()
+	ca.workingDirectory, err = coreutils.GetWorkingDirectory()
 	if err != nil {
 		return err
 	}
-	log.Debug("Working directory set to:", com.workingDirectory)
-	if err = com.setArtifactoryAuth(); err != nil {
+	log.Debug("Working directory set to:", ca.workingDirectory)
+	if err = ca.setArtifactoryAuth(); err != nil {
 		return err
 	}
 
-	com.npmAuth, com.registry, err = commandUtils.GetArtifactoryNpmRepoDetails(repo, &com.authArtDetails)
+	ca.npmAuth, ca.registry, err = commandUtils.GetArtifactoryNpmRepoDetails(repo, &ca.authArtDetails)
 	if err != nil {
 		return err
 	}
 
-	return com.setRestoreNpmrcFunc()
+	return ca.setRestoreNpmrcFunc()
 }
 
-func (com *CommonArgs) setJsonOutput() error {
-	jsonOutput, err := npm.ConfigGet(com.npmArgs, "json", com.executablePath)
+func (ca *CommonArgs) setJsonOutput() error {
+	jsonOutput, err := npm.ConfigGet(ca.npmArgs, "json", ca.executablePath)
 	if err != nil {
 		return err
 	}
 
 	// In case of --json=<not boolean>, the value of json is set to 'true', but the result from the command is not 'true'
-	com.jsonOutput = jsonOutput != "false"
+	ca.jsonOutput = jsonOutput != "false"
 	return nil
 }
 
-func (com *CommonArgs) setArtifactoryAuth() error {
-	authArtDetails, err := com.serverDetails.CreateArtAuthConfig()
+func (ca *CommonArgs) setArtifactoryAuth() error {
+	authArtDetails, err := ca.serverDetails.CreateArtAuthConfig()
 	if err != nil {
 		return err
 	}
 	if authArtDetails.GetSshAuthHeaders() != nil {
 		return errorutils.CheckErrorf("SSH authentication is not supported in this command")
 	}
-	com.authArtDetails = authArtDetails
+	ca.authArtDetails = authArtDetails
 	return nil
 }
 
 // In order to make sure the npm resolves artifacts from Artifactory we create a .npmrc file in the project dir.
 // If such a file exists we back it up as npmrcBackupFileName.
-func (com *CommonArgs) createTempNpmrc() error {
+func (ca *CommonArgs) createTempNpmrc() error {
 	log.Debug("Creating project .npmrc file.")
-	data, err := npm.GetConfigList(com.npmArgs, com.executablePath)
+	data, err := npm.GetConfigList(ca.npmArgs, ca.executablePath)
 	if err != nil {
 		return err
 	}
-	configData, err := com.prepareConfigData(data)
+	configData, err := ca.prepareConfigData(data)
 	if err != nil {
 		return errorutils.CheckError(err)
 	}
 
-	if err = removeNpmrcIfExists(com.workingDirectory); err != nil {
+	if err = removeNpmrcIfExists(ca.workingDirectory); err != nil {
 		return err
 	}
 
-	return errorutils.CheckError(os.WriteFile(filepath.Join(com.workingDirectory, npmrcFileName), configData, 0600))
+	return errorutils.CheckError(os.WriteFile(filepath.Join(ca.workingDirectory, npmrcFileName), configData, 0600))
 }
 
 // This func transforms "npm config list" result to key=val list of values that can be set to .npmrc file.
 // it filters out any nil value key, changes registry and scope registries to Artifactory url and adds Artifactory authentication to the list
-func (com *CommonArgs) prepareConfigData(data []byte) ([]byte, error) {
+func (ca *CommonArgs) prepareConfigData(data []byte) ([]byte, error) {
 	var filteredConf []string
-	configString := string(data) + "\n" + com.npmAuth
+	configString := string(data) + "\n" + ca.npmAuth
 	scanner := bufio.NewScanner(strings.NewReader(configString))
-
 	for scanner.Scan() {
 		currOption := scanner.Text()
-		if currOption != "" {
-			splitOption := strings.SplitN(currOption, "=", 2)
-			key := strings.TrimSpace(splitOption[0])
-			if len(splitOption) == 2 && isValidKey(key) {
-				value := strings.TrimSpace(splitOption[1])
-				if key == "_auth" {
-					// Set "NPM_CONFIG__AUTH" environment variable to allow authentication with Artifactory when running postinstall scripts on subdirectories.
-					if err := os.Setenv(npmConfigAuthEnv, value); err != nil {
-						return nil, errorutils.CheckError(err)
-					}
-				} else if strings.HasPrefix(value, "[") && strings.HasSuffix(value, "]") {
-					filteredConf = addArrayConfigs(filteredConf, key, value)
-				} else {
-					filteredConf = append(filteredConf, currOption, "\n")
-				}
-			} else if strings.HasPrefix(splitOption[0], "@") {
-				// Override scoped registries (@scope = xyz)
-				filteredConf = append(filteredConf, splitOption[0], " = ", com.registry, "\n")
-			}
+		if currOption == "" {
+			continue
+		}
+		filteredLine, err := ca.processConfigLine(currOption)
+		if err != nil {
+			return nil, errorutils.CheckError(err)
+		}
+		if filteredLine != "" {
+			filteredConf = append(filteredConf, filteredLine)
 		}
 	}
 	if err := scanner.Err(); err != nil {
 		return nil, errorutils.CheckError(err)
 	}
 
-	filteredConf = append(filteredConf, "json = ", strconv.FormatBool(com.jsonOutput), "\n")
-	filteredConf = append(filteredConf, "registry = ", com.registry, "\n")
+	filteredConf = append(filteredConf, "json = ", strconv.FormatBool(ca.jsonOutput), "\n")
+	filteredConf = append(filteredConf, "registry = ", ca.registry, "\n")
 	return []byte(strings.Join(filteredConf, "")), nil
 }
 
-func (com *CommonArgs) setRestoreNpmrcFunc() error {
-	restoreNpmrcFunc, err := commandUtils.BackupFile(filepath.Join(com.workingDirectory, npmrcFileName), filepath.Join(com.workingDirectory, npmrcBackupFileName))
+func (ca *CommonArgs) processConfigLine(configLine string) (filteredLine string, err error) {
+	splitOption := strings.SplitN(configLine, "=", 2)
+	key := strings.TrimSpace(splitOption[0])
+	validLine := len(splitOption) == 2 && isValidKey(key)
+	if !validLine {
+		if strings.HasPrefix(splitOption[0], "@") {
+			// Override scoped registries (@scope = xyz)
+			return fmt.Sprintf("%s = %s\n", splitOption[0], ca.registry), nil
+		}
+		return
+	}
+	value := strings.TrimSpace(splitOption[1])
+	if key == "_auth" {
+		return "", ca.setNpmConfigAuthEnv(value)
+	}
+	if strings.HasPrefix(value, "[") && strings.HasSuffix(value, "]") {
+		return addArrayConfigs(key, value), nil
+	}
+
+	return fmt.Sprintf("%s\n", configLine), err
+}
+
+func (ca *CommonArgs) setNpmConfigAuthEnv(value string) error {
+	// Check if the npm version is bigger or equal to 9.3.1
+	if ca.npmVersion.Compare(npmVersionForLegacyEnv) <= 0 {
+		// Get registry name without the protocol name but including the '//'
+		registryWithoutProtocolName := ca.registry[strings.Index(ca.registry, "://")+1:]
+		// Set "npm_config_//<registry-url>:_auth" environment variable to allow authentication with Artifactory
+		scopedRegistryEnv := fmt.Sprintf(npmConfigAuthEnv, registryWithoutProtocolName)
+		return os.Setenv(scopedRegistryEnv, value)
+	}
+	// Set "npm_config__auth" environment variable to allow authentication with Artifactory when running postinstall scripts on subdirectories.
+	// For Legacy NPM version < 9.3.1
+	return os.Setenv(npmLegacyConfigAuthEnv, value)
+}
+
+func (ca *CommonArgs) setRestoreNpmrcFunc() error {
+	restoreNpmrcFunc, err := commandUtils.BackupFile(filepath.Join(ca.workingDirectory, npmrcFileName), filepath.Join(ca.workingDirectory, npmrcBackupFileName))
 	if err != nil {
 		return err
 	}
-	com.restoreNpmrcFunc = func() error {
+	ca.restoreNpmrcFunc = func() error {
 		if unsetEnvErr := os.Unsetenv(npmConfigAuthEnv); unsetEnvErr != nil {
 			log.Warn("Couldn't unset", npmConfigAuthEnv)
 		}

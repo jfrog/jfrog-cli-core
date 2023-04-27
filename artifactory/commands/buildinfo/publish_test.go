c85dc2a660d79dba1b0d628604cdf94b2c3c528a
Michael Sverdlov <michaelsv@jfrog.com>
2023-04-27 15:53:28 +0300

    update
    Signed-off-by: Michael Sverdlov <michaelsv@jfrog.com>

--- artifactory/commands/buildinfo/publish_test.go
+++ artifactory/commands/buildinfo/publish_test.go
@@ -11,8 +11,7 @@ import (
 )
 
 func TestPrintBuildInfoLink(t *testing.T) {
-	timeNow := time.Now()
-	buildTime := strconv.FormatInt(timeNow.UnixNano()/1000000, 10)
+	buildTime := strconv.FormatInt(time.Now().UnixNano()/1000000, 10)
 	var linkTypes = []struct {
 		majorVersion  int
 		buildTime     time.Time
@@ -21,23 +20,23 @@ func TestPrintBuildInfoLink(t *testing.T) {
 		expected      string
 	}{
 		// Test platform URL
-		{5, timeNow, artifactoryUtils.NewBuildConfiguration("test", "1", "6", "cli"),
+		{5, time.Now(), artifactoryUtils.NewBuildConfiguration("test", "1", "6", "cli"),
 			config.ServerDetails{Url: "http://localhost:8081/"}, "http://localhost:8081/artifactory/webapp/#/builds/test/1"},
-		{6, timeNow, artifactoryUtils.NewBuildConfiguration("test", "1", "6", "cli"),
+		{6, time.Now(), artifactoryUtils.NewBuildConfiguration("test", "1", "6", "cli"),
 			config.ServerDetails{Url: "http://localhost:8081/"}, "http://localhost:8081/artifactory/webapp/#/builds/test/1"},
-		{7, timeNow, artifactoryUtils.NewBuildConfiguration("test", "1", "6", ""),
+		{7, time.Now(), artifactoryUtils.NewBuildConfiguration("test", "1", "6", ""),
 			config.ServerDetails{Url: "http://localhost:8082/"}, "http://localhost:8082/ui/builds/test/1/" + buildTime + "/published?buildRepo=artifactory-build-info"},
-		{7, timeNow, artifactoryUtils.NewBuildConfiguration("test", "1", "6", "cli"),
+		{7, time.Now(), artifactoryUtils.NewBuildConfiguration("test", "1", "6", "cli"),
 			config.ServerDetails{Url: "http://localhost:8082/"}, "http://localhost:8082/ui/builds/test/1/" + buildTime + "/published?buildRepo=cli-build-info&projectKey=cli"},
 
 		// Test Artifactory URL
-		{5, timeNow, artifactoryUtils.NewBuildConfiguration("test", "1", "6", "cli"),
+		{5, time.Now(), artifactoryUtils.NewBuildConfiguration("test", "1", "6", "cli"),
 			config.ServerDetails{ArtifactoryUrl: "http://localhost:8081/artifactory"}, "http://localhost:8081/artifactory/webapp/#/builds/test/1"},
-		{6, timeNow, artifactoryUtils.NewBuildConfiguration("test", "1", "6", "cli"),
+		{6, time.Now(), artifactoryUtils.NewBuildConfiguration("test", "1", "6", "cli"),
 			config.ServerDetails{ArtifactoryUrl: "http://localhost:8081/artifactory/"}, "http://localhost:8081/artifactory/webapp/#/builds/test/1"},
-		{7, timeNow, artifactoryUtils.NewBuildConfiguration("test", "1", "6", ""),
+		{7, time.Now(), artifactoryUtils.NewBuildConfiguration("test", "1", "6", ""),
 			config.ServerDetails{ArtifactoryUrl: "http://localhost:8082/artifactory"}, "http://localhost:8082/ui/builds/test/1/" + buildTime + "/published?buildRepo=artifactory-build-info"},
-		{7, timeNow, artifactoryUtils.NewBuildConfiguration("test", "1", "6", "cli"),
+		{7, time.Now(), artifactoryUtils.NewBuildConfiguration("test", "1", "6", "cli"),
 			config.ServerDetails{ArtifactoryUrl: "http://localhost:8082/artifactory/"}, "http://localhost:8082/ui/builds/test/1/" + buildTime + "/published?buildRepo=cli-build-info&projectKey=cli"},
 	}
 

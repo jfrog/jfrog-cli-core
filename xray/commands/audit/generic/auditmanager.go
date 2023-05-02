71370e6052ebee3cf34c0078cf956accf6ca3606
Omer Zidkoni <50792403+omerzi@users.noreply.github.com>
2023-05-02 17:30:18 +0300

    Fix binary scan request

--- xray/commands/audit/generic/auditmanager.go
+++ xray/commands/audit/generic/auditmanager.go
@@ -26,7 +26,7 @@ import (
 )
 
 type Params struct {
-	xrayGraphScanParams services.XrayGraphScanParams
+	xrayGraphScanParams *services.XrayGraphScanParams
 	serverDetails       *config.ServerDetails
 	progress            ioUtils.ProgressMgr
 	dependencyTrees     []*services.GraphNode
@@ -34,6 +34,8 @@ type Params struct {
 	excludeTestDeps     bool
 	insecureTls         bool
 	useWrapper          bool
+	fixableOnly         bool
+	minSeverityFilter   string
 	depsRepo            string
 	releasesRepo        string
 	requirementsFile    string
@@ -52,7 +54,7 @@ func (params *Params) InstallFunc() func(tech string) error {
 	return params.installFunc
 }
 
-func (params *Params) XrayGraphScanParams() services.XrayGraphScanParams {
+func (params *Params) XrayGraphScanParams() *services.XrayGraphScanParams {
 	return params.xrayGraphScanParams
 }
 
@@ -104,7 +106,7 @@ func (params *Params) XrayVersion() string {
 	return params.xrayVersion
 }
 
-func (params *Params) SetXrayGraphScanParams(xrayGraphScanParams services.XrayGraphScanParams) *Params {
+func (params *Params) SetXrayGraphScanParams(xrayGraphScanParams *services.XrayGraphScanParams) *Params {
 	params.xrayGraphScanParams = xrayGraphScanParams
 	return params
 }
@@ -171,6 +173,24 @@ func (params *Params) SetReleasesRepo(releasesRepo string) *Params {
 
 func (params *Params) SetInstallFunc(installFunc func(tech string) error) *Params {
 	params.installFunc = installFunc
+	return params
+}
+
+func (params *Params) FixableOnly() bool {
+	return params.fixableOnly
+}
+
+func (params *Params) SetFixableOnly(fixable bool) *Params {
+	params.fixableOnly = fixable
+	return params
+}
+
+func (params *Params) MinSeverityFilter() string {
+	return params.minSeverityFilter
+}
+
+func (params *Params) SetMinSeverityFilter(minSeverityFilter string) *Params {
+	params.minSeverityFilter = minSeverityFilter
 	return params
 }
 
@@ -258,7 +278,14 @@ func doAudit(params *Params) (results []services.ScanResponse, isMultipleRoot bo
 			errorList.WriteString(fmt.Sprintf("audit failed while building %s dependency tree:\n%s\n", tech, e.Error()))
 			continue
 		}
-		techResults, e := audit.Audit(dependencyTrees, params.xrayGraphScanParams, params.serverDetails, params.progress, tech, params.xrayVersion)
+
+		scanGraphParams := xraycommands.NewScanGraphParams().
+			SetServerDetails(params.serverDetails).
+			SetXrayGraphScanParams(params.xrayGraphScanParams).
+			SetXrayVersion(params.xrayVersion).
+			SetFixableOnly(params.fixableOnly).
+			SetSeverityLevel(params.minSeverityFilter)
+		techResults, e := audit.Audit(dependencyTrees, params.progress, tech, scanGraphParams)
 		if e != nil {
 			errorList.WriteString(fmt.Sprintf("'%s' audit request failed:\n%s\n", tech, e.Error()))
 			continue

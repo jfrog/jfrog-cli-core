6f94aa3ca24f9f24780807cd15ed452ffd418ce5
Asaf Gabai <asafg@jfrog.com>
2023-05-30 11:34:25 +0300

    Fixed the Xray ID generated for the root of scoped packages.

--- xray/audit/yarn/yarn_test.go
+++ xray/audit/yarn/yarn_test.go
@@ -10,16 +10,16 @@ import (
 
 func TestParseYarnDependenciesList(t *testing.T) {
 	yarnDependencies := map[string]*biutils.YarnDependency{
-		"pack1@npm:1.0.0": {Value: "pack1@npm:1.0.0", Details: biutils.YarnDepDetails{Version: "1.0.0", Dependencies: []biutils.YarnDependencyPointer{{Locator: "pack4@npm:4.0.0"}}}},
-		"pack2@npm:2.0.0": {Value: "pack2@npm:2.0.0", Details: biutils.YarnDepDetails{Version: "2.0.0", Dependencies: []biutils.YarnDependencyPointer{{Locator: "pack4@npm:4.0.0"}, {Locator: "pack5@npm:5.0.0"}}}},
-		"pack3@npm:3.0.0": {Value: "pack3@npm:3.0.0", Details: biutils.YarnDepDetails{Version: "3.0.0", Dependencies: []biutils.YarnDependencyPointer{{Locator: "pack1@virtual:c192f6b3b32cd5d11a443144e162ec3bc#npm:1.0.0"}, {Locator: "pack2@npm:2.0.0"}}}},
-		"pack4@npm:4.0.0": {Value: "pack4@npm:4.0.0", Details: biutils.YarnDepDetails{Version: "4.0.0"}},
-		"pack5@npm:5.0.0": {Value: "pack5@npm:5.0.0", Details: biutils.YarnDepDetails{Version: "5.0.0", Dependencies: []biutils.YarnDependencyPointer{{Locator: "pack2@npm:2.0.0"}}}},
+		"pack1@npm:1.0.0":        {Value: "pack1@npm:1.0.0", Details: biutils.YarnDepDetails{Version: "1.0.0", Dependencies: []biutils.YarnDependencyPointer{{Locator: "pack4@npm:4.0.0"}}}},
+		"pack2@npm:2.0.0":        {Value: "pack2@npm:2.0.0", Details: biutils.YarnDepDetails{Version: "2.0.0", Dependencies: []biutils.YarnDependencyPointer{{Locator: "pack4@npm:4.0.0"}, {Locator: "pack5@npm:5.0.0"}}}},
+		"@jfrog/pack3@npm:3.0.0": {Value: "@jfrog/pack3@npm:3.0.0", Details: biutils.YarnDepDetails{Version: "3.0.0", Dependencies: []biutils.YarnDependencyPointer{{Locator: "pack1@virtual:c192f6b3b32cd5d11a443144e162ec3bc#npm:1.0.0"}, {Locator: "pack2@npm:2.0.0"}}}},
+		"pack4@npm:4.0.0":        {Value: "pack4@npm:4.0.0", Details: biutils.YarnDepDetails{Version: "4.0.0"}},
+		"pack5@npm:5.0.0":        {Value: "pack5@npm:5.0.0", Details: biutils.YarnDepDetails{Version: "5.0.0", Dependencies: []biutils.YarnDependencyPointer{{Locator: "pack2@npm:2.0.0"}}}},
 	}
 
-	packageInfo := &biutils.PackageInfo{Name: "pack3", Version: "3.0.0"}
+	rootXrayId := "npm://@jfrog/pack3:3.0.0"
 	expectedTree := &xrayUtils.GraphNode{
-		Id: "npm://pack3:3.0.0",
+		Id: rootXrayId,
 		Nodes: []*xrayUtils.GraphNode{
 			{Id: "npm://pack1:1.0.0",
 				Nodes: []*xrayUtils.GraphNode{
@@ -36,7 +36,7 @@ func TestParseYarnDependenciesList(t *testing.T) {
 		},
 	}
 
-	xrayDependenciesTree := parseYarnDependenciesMap(yarnDependencies, packageInfo)
+	xrayDependenciesTree := parseYarnDependenciesMap(yarnDependencies, rootXrayId)
 
 	equals := tests.CompareTree(expectedTree, xrayDependenciesTree)
 	if !equals {

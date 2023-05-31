c12e9ffe3a221ecfbe90d008ec1aad8952733d1a
yahavi <yahavi@jfrog.com>
2023-05-31 14:55:02 +0300

    Bugfix - File transfer stops unexpectedly

--- artifactory/commands/transferfiles/utils.go
+++ artifactory/commands/transferfiles/utils.go
@@ -131,24 +131,8 @@ func runAql(ctx context.Context, sourceRtDetails *config.ServerDetails, query st
 	}
 
 	result = &serviceUtils.AqlSearchResult{}
-	if err = json.Unmarshal(respBody, result); err != nil {
-		return nil, errorutils.CheckError(err)
-	}
-
-	return filterAqlResults(result), nil
-}
-
-// Filter out the root folder in the repository.
-// unfilteredResults - Unfiltered AQL search results
-func filterAqlResults(unfilteredResults *serviceUtils.AqlSearchResult) *serviceUtils.AqlSearchResult {
-	results := &serviceUtils.AqlSearchResult{}
-	for _, result := range unfilteredResults.Results {
-		if result.Path == "." && result.Name == "." {
-			continue
-		}
-		results.Results = append(results.Results, result)
-	}
-	return results
+	err = json.Unmarshal(respBody, result)
+	return result, errorutils.CheckError(err)
 }
 
 func createTargetAuth(targetRtDetails *config.ServerDetails, proxyKey string) api.TargetAuth {

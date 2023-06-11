d003935e88750561cb04675985a7dec6d0a846cd
Sara Omari <114062096+sarao1310@users.noreply.github.com>
2023-06-11 16:58:50 +0300

    New `--include` option for `jf rt search` (#775)
    * support a new search flag --include

--- artifactory/utils/search.go
+++ artifactory/utils/search.go
@@ -13,15 +13,21 @@ import (
 )
 
 type SearchResult struct {
-	Path     string              `json:"path,omitempty"`
-	Type     string              `json:"type,omitempty"`
-	Size     int64               `json:"size,omitempty"`
-	Created  string              `json:"created,omitempty"`
-	Modified string              `json:"modified,omitempty"`
-	Sha1     string              `json:"sha1,omitempty"`
-	Sha256   string              `json:"sha256,omitempty"`
-	Md5      string              `json:"md5,omitempty"`
-	Props    map[string][]string `json:"props,omitempty"`
+	Path         string              `json:"path,omitempty"`
+	Type         string              `json:"type,omitempty"`
+	Size         int64               `json:"size,omitempty"`
+	Created      string              `json:"created,omitempty"`
+	Modified     string              `json:"modified,omitempty"`
+	Sha1         string              `json:"sha1,omitempty"`
+	Sha256       string              `json:"sha256,omitempty"`
+	Md5          string              `json:"md5,omitempty"`
+	OriginalMd5  string              `json:"original_md5,omitempty"`
+	ModifiedBy   string              `json:"modified_by,omitempty"`
+	Updated      string              `json:"updated,omitempty"`
+	CreatedBy    string              `json:"created_by,omitempty"`
+	OriginalSha1 string              `json:"original_sha1,omitempty"`
+	Depth        int                 `json:"depth,omitempty"`
+	Props        map[string][]string `json:"props,omitempty"`
 }
 
 func PrintSearchResults(reader *content.ContentReader) error {
@@ -88,13 +94,18 @@ func AqlResultToSearchResult(readers []*content.ContentReader) (contentReader *c
 			tempResult.Sha1 = searchResult.Actual_Sha1
 			tempResult.Sha256 = searchResult.Sha256
 			tempResult.Md5 = searchResult.Actual_Md5
+			tempResult.ModifiedBy = searchResult.ModifiedBy
+			tempResult.Updated = searchResult.Updated
+			tempResult.CreatedBy = searchResult.CreatedBy
+			tempResult.OriginalMd5 = searchResult.OriginalMd5
+			tempResult.Depth = searchResult.Depth
 			tempResult.Props = make(map[string][]string, len(searchResult.Properties))
 			for _, prop := range searchResult.Properties {
 				tempResult.Props[prop.Key] = append(tempResult.Props[prop.Key], prop.Value)
 			}
 			writer.Write(*tempResult)
 		}
-		if err := reader.GetError(); err != nil {
+		if err = reader.GetError(); err != nil {
 			return nil, err
 		}
 		reader.Reset()
@@ -106,6 +117,7 @@ func AqlResultToSearchResult(readers []*content.ContentReader) (contentReader *c
 func GetSearchParams(f *spec.File) (searchParams services.SearchParams, err error) {
 	searchParams = services.NewSearchParams()
 	searchParams.CommonParams, err = f.ToCommonParams()
+	searchParams.Include = f.GetInclude()
 	if err != nil {
 		return
 	}
@@ -126,6 +138,7 @@ func GetSearchParams(f *spec.File) (searchParams services.SearchParams, err erro
 		return
 	}
 	searchParams.Transitive, err = f.IsTransitive(false)
+	searchParams.Include = f.GetInclude()
 	return
 }
 

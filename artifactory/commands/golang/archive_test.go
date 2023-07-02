c80055725be150774f340815504cd8b47885f9a6
sarao <sarao@jfrog.com>
2023-07-02 08:56:54 +0300

    add --exclusions flag to go-publish

--- artifactory/commands/golang/archive_test.go
+++ artifactory/commands/golang/archive_test.go
@@ -42,27 +42,11 @@ func TestArchiveProject(t *testing.T) {
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("Expecting: %v, Got: %v", expected, actual)
 	}
-	tests.RenamePath(dotGitPath, filepath.Join(baseDir, originalFolder), t)
-}
-
-func TestArchiveProjectWithExclusions(t *testing.T) {
-	log.SetDefaultLogger()
-	if coreutils.IsWindows() {
-		t.Skip("Skipping archive test...")
-	}
-	pwd, err := os.Getwd()
-	if err != nil {
-		t.Error(err)
-	}
-
-	buff := &bytes.Buffer{}
-	originalFolder := "test_.git_suffix"
-	baseDir, dotGitPath := tests.PrepareDotGitDir(t, originalFolder, "testdata")
 	if err = archiveProject(buff, filepath.Join(pwd, "testdata"), "myproject.com/module/name", "v1.0.0", []string{"testdata/dir2/*"}); err != nil {
 		t.Error(err)
 	}
-	expected := map[utils.Algorithm]string{utils.MD5: "28617d6e74fce3dd2bab21b1bd65009b", utils.SHA1: "410814fbf21afdfb9c5b550151a51c2e986447fa", utils.SHA256: "e877c07315d6d3ad69139035defc08c04b400b36cd069b35ea3c2960424f2dc6"}
-	actual, err := utils.CalcChecksums(buff)
+	expected = map[utils.Algorithm]string{utils.MD5: "28617d6e74fce3dd2bab21b1bd65009b", utils.SHA1: "410814fbf21afdfb9c5b550151a51c2e986447fa", utils.SHA256: "e877c07315d6d3ad69139035defc08c04b400b36cd069b35ea3c2960424f2dc6"}
+	actual, err = utils.CalcChecksums(buff)
 	if err != nil {
 		t.Error(err)
 	}

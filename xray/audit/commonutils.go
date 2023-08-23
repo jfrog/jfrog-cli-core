93c36c70e3119a455e7f4e9dd8b44ebabb7e3d15
Michael Sverdlov <sverdlov93@gmail.com>
2023-08-23 20:06:17 +0300

    Improve SCA and Advanced Security error results (#909)

--- xray/audit/commonutils.go
+++ xray/audit/commonutils.go
@@ -53,12 +53,7 @@ func buildXrayDependencyTree(treeHelper map[string][]string, impactPath []string
 	return xrDependencyTree
 }
 
-func Audit(modulesDependencyTrees []*xrayUtils.GraphNode, progress ioUtils.ProgressMgr, technology coreutils.Technology, scanGraphParams *xraycommands.ScanGraphParams) (results []services.ScanResponse, err error) {
-	if len(modulesDependencyTrees) == 0 {
-		err = errorutils.CheckErrorf("No dependencies were found. Please try to build your project and re-run the audit command.")
-		return
-	}
-
+func RunXrayDependenciesTreeScanGraph(modulesDependencyTrees []*xrayUtils.GraphNode, progress ioUtils.ProgressMgr, technology coreutils.Technology, scanGraphParams *xraycommands.ScanGraphParams) (results []services.ScanResponse, err error) {
 	if progress != nil {
 		progress.SetHeadlineMsg("Scanning for vulnerabilities")
 	}

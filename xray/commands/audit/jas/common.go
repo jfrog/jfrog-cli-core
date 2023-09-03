bfd144ef71dd8d7e0d50aca2e0d5993c50dd19e5
Michael Sverdlov <michaelsv@jfrog.com>
2023-09-03 19:31:25 +0300

    copy dir
    Signed-off-by: Michael Sverdlov <michaelsv@jfrog.com>

--- xray/commands/audit/jas/common.go
+++ xray/commands/audit/jas/common.go
@@ -2,15 +2,20 @@ package jas
 
 import (
 	"errors"
+	rtutils "github.com/jfrog/jfrog-cli-core/v2/artifactory/utils"
 	"github.com/jfrog/jfrog-cli-core/v2/utils/config"
+	"github.com/jfrog/jfrog-cli-core/v2/utils/coreutils"
 	"github.com/jfrog/jfrog-cli-core/v2/xray/utils"
 	"github.com/jfrog/jfrog-client-go/utils/errorutils"
 	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
+	"github.com/jfrog/jfrog-client-go/xray/services"
 	"github.com/owenrumney/go-sarif/v2/sarif"
+	"github.com/stretchr/testify/assert"
 	"gopkg.in/yaml.v3"
 	"os"
 	"path/filepath"
 	"strings"
+	"testing"
 )
 
 var (
@@ -139,4 +144,35 @@ func hideSecret(secret string) string {
 		return "***"
 	}
 	return secret[:3] + strings.Repeat("*", 12)
+}
+
+var FakeServerDetails = config.ServerDetails{
+	Url:      "platformUrl",
+	Password: "password",
+	User:     "user",
+}
+
+var FakeBasicXrayResults = []services.ScanResponse{
+	{
+		ScanId: "scanId_1",
+		Vulnerabilities: []services.Vulnerability{
+			{IssueId: "issueId_1", Technology: coreutils.Pipenv.ToString(),
+				Cves:       []services.Cve{{Id: "testCve1"}, {Id: "testCve2"}, {Id: "testCve3"}},
+				Components: map[string]services.Component{"issueId_1_direct_dependency": {}, "issueId_3_direct_dependency": {}}},
+		},
+		Violations: []services.Violation{
+			{IssueId: "issueId_2", Technology: coreutils.Pipenv.ToString(),
+				Cves:       []services.Cve{{Id: "testCve4"}, {Id: "testCve5"}},
+				Components: map[string]services.Component{"issueId_2_direct_dependency": {}, "issueId_4_direct_dependency": {}}},
+		},
+	},
+}
+
+func InitJasTest(t *testing.T, workingDirs ...string) (*JasScanner, func()) {
+	assert.NoError(t, rtutils.DownloadAnalyzerManagerIfNeeded())
+	scanner, err := NewJasScanner(workingDirs, &FakeServerDetails)
+	assert.NoError(t, err)
+	return scanner, func() {
+		assert.NoError(t, scanner.ScannerDirCleanupFunc())
+	}
 }

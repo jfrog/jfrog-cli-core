e7f45e6e436e5c341cb71134d34cad047adba4c3
Michael Sverdlov <michaelsv@jfrog.com>
2023-09-04 19:40:43 +0300

    copy dir
    Signed-off-by: Michael Sverdlov <michaelsv@jfrog.com>

--- xray/scangraph/scangraph.go
+++ xray/scangraph/scangraph.go
@@ -9,8 +9,8 @@ import (
 )
 
 const (
-	GraphScanMinXrayVersion2 = "3.29.0"
-	ScanTypeMinXrayVersion   = "3.37.2"
+	GraphScanMinXrayVersion = "3.29.0"
+	ScanTypeMinXrayVersion  = "3.37.2"
 )
 
 func RunScanGraphAndGetResults(params *ScanGraphParams) (*services.ScanResponse, error) {

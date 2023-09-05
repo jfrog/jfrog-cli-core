31f360f5d8e800cdc002b7f0221209ffb29a2229
Michael Sverdlov <michaelsv@jfrog.com>
2023-09-05 20:00:28 +0300

    copy dir
    Signed-off-by: Michael Sverdlov <michaelsv@jfrog.com>

--- xray/utils/analyzermanager.go
+++ xray/utils/analyzermanager.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"errors"
 	"fmt"
+	"github.com/jfrog/gofrog/version"
 	"os"
 	"os/exec"
 	"path"
@@ -41,8 +42,8 @@ const (
 	EntitlementsMinVersion           = "3.66.5"
 	ApplicabilityFeatureId           = "contextual_analysis"
 	AnalyzerManagerZipName           = "analyzerManager.zip"
-	AnalyzerManagerVersion           = "1.2.4.1953469"
-	MinAnalyzerManagerVersionForSast = "1.3"
+	defaultAnalyzerManagerVersion    = "1.2.4.1953469"
+	minAnalyzerManagerVersionForSast = "1.3"
 	analyzerManagerDownloadPath      = "xsc-gen-exe-analyzer-manager-local/v1"
 	analyzerManagerDirName           = "analyzerManager"
 	analyzerManagerExecutableName    = "analyzerManager"
@@ -142,7 +143,18 @@ func GetAnalyzerManagerDownloadPath() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return path.Join(analyzerManagerDownloadPath, AnalyzerManagerVersion, osAndArc, AnalyzerManagerZipName), nil
+	return path.Join(analyzerManagerDownloadPath, GetAnalyzerManagerVersion(), osAndArc, AnalyzerManagerZipName), nil
+}
+
+func GetAnalyzerManagerVersion() string {
+	if analyzerManagerVersion, exists := os.LookupEnv("JFROG_CLI_ANALYZER_MANAGER_VERSION"); exists {
+		return analyzerManagerVersion
+	}
+	return defaultAnalyzerManagerVersion
+}
+
+func IsSastSupported() bool {
+	return version.NewVersion(GetAnalyzerManagerVersion()).AtLeast(minAnalyzerManagerVersionForSast)
 }
 
 func GetAnalyzerManagerDirAbsolutePath() (string, error) {

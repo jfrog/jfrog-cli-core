e4135603d23bb25f47b6b0c81456defbbebd17e5
attiasas <assafattias93@gmail.com>
2023-09-20 14:54:21 +0300

    Show finding in Sast table

--- xray/utils/resultstable.go
+++ xray/utils/resultstable.go
@@ -420,6 +420,7 @@ func prepareSast(sasts []*sarif.Run, isTable bool) []formats.SourceCodeRow {
 					formats.SourceCodeRow{
 						SeverityDetails:    formats.SeverityDetails{Severity: currSeverity.printableTitle(isTable), SeverityNumValue: currSeverity.NumValue()},
 						ScannerDescription: scannerDescription,
+						Finding: GetResultMsgText(sastResult),
 						Location: formats.Location{
 							File:        GetRelativeLocationFileName(location, sastRun.Invocations),
 							StartLine:   GetLocationStartLine(location),

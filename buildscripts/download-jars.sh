63d1f76936bbf79eede35e149a5b1daf18aeaf3e
Omer Zidkoni <omerz@jfrog.com>
2023-11-13 09:07:18 +0200

    Update jars

--- buildscripts/download-jars.sh
+++ buildscripts/download-jars.sh
@@ -8,7 +8,7 @@
 
 # Once you have updated the versions mentioned below, please execute this script from the root directory of the jfrog-cli-core to ensure the JAR files are updated.
 GRADLE_DEP_TREE_VERSION="3.0.1"
-MAVEN_DEP_TREE_VERSION="1.0.0"
+MAVEN_DEP_TREE_VERSION="1.0.2"
 
 curl -fL https://releases.jfrog.io/artifactory/oss-release-local/com/jfrog/gradle-dep-tree/${GRADLE_DEP_TREE_VERSION}/gradle-dep-tree-${GRADLE_DEP_TREE_VERSION}.jar -o xray/commands/audit/sca/java/gradle-dep-tree.jar
 curl -fL https://releases.jfrog.io/artifactory/oss-release-local/com/jfrog/maven-dep-tree/${MAVEN_DEP_TREE_VERSION}/maven-dep-tree-${MAVEN_DEP_TREE_VERSION}.jar -o xray/commands/audit/sca/java/maven-dep-tree.jar

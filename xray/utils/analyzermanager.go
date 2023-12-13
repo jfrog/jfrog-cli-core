9fe3a38c1aa52350d96dfe41fdd3c23448a13c6c
Omer Zidkoni <omerz@jfrog.com>
2023-12-13 15:44:26 +0200

    Merge branch 'dev' into parse-only-json-in-mvn-dep-tree-fix

--- xray/utils/analyzermanager.go
+++ xray/utils/analyzermanager.go
@@ -20,7 +20,7 @@ const (
 	EntitlementsMinVersion                    = "3.66.5"
 	ApplicabilityFeatureId                    = "contextual_analysis"
 	AnalyzerManagerZipName                    = "analyzerManager.zip"
-	defaultAnalyzerManagerVersion             = "1.6.1"
+	defaultAnalyzerManagerVersion             = "1.6.0"
 	analyzerManagerDownloadPath               = "xsc-gen-exe-analyzer-manager-local/v1"
 	analyzerManagerDirName                    = "analyzerManager"
 	analyzerManagerExecutableName             = "analyzerManager"

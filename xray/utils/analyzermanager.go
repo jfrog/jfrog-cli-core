b147c5e20c86ed03149354e4cebfe88bd88c708c
Or Toren <129293360+orto17@users.noreply.github.com>
2023-12-20 18:27:19 +0200

    upgraded analyzer manager to version 1.6.3 (#1083)

--- xray/utils/analyzermanager.go
+++ xray/utils/analyzermanager.go
@@ -20,7 +20,7 @@ const (
 	EntitlementsMinVersion                    = "3.66.5"
 	ApplicabilityFeatureId                    = "contextual_analysis"
 	AnalyzerManagerZipName                    = "analyzerManager.zip"
-	defaultAnalyzerManagerVersion             = "1.6.2"
+	defaultAnalyzerManagerVersion             = "1.6.3"
 	analyzerManagerDownloadPath               = "xsc-gen-exe-analyzer-manager-local/v1"
 	analyzerManagerDirName                    = "analyzerManager"
 	analyzerManagerExecutableName             = "analyzerManager"

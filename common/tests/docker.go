286b0a5b186f8b7f7e7fcca82f4a2f93d8cde189
attiasas <assafattias93@gmail.com>
2023-12-28 13:45:58 +0200

    start clean

--- common/tests/docker.go
+++ common/tests/docker.go
@@ -4,19 +4,10 @@ import (
 	"io"
 	"os/exec"
 	"path"
-	// "path/filepath"
-	// "strings"
 	"testing"
 
-	// "github.com/jfrog/jfrog-cli-core/v2/common/spec"
-
 	gofrogcmd "github.com/jfrog/gofrog/io"
-	// "github.com/jfrog/jfrog-cli-core/v2/artifactory/commands/generic"
-	// "github.com/jfrog/jfrog-cli-core/v2/artifactory/utils"
 	"github.com/jfrog/jfrog-cli-core/v2/artifactory/utils/container"
-	// "github.com/jfrog/jfrog-cli-core/v2/utils/config"
-	// "github.com/jfrog/jfrog-client-go/utils/io/httputils"
-	// "github.com/jfrog/jfrog-client-go/utils/log"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -199,61 +190,3 @@ func DeleteTestContainer(t *testing.T, containerName string, containerManagerTyp
 	containerDelete := NewDeleteContainer(containerName, containerManagerType)
 	assert.NoError(t, gofrogcmd.RunCmd(containerDelete))
 }
-
-// ---- CLI specific, remove ----
-
-// func BuildTestImage(imageName, dockerfileName, repo string, containerManagerType container.ContainerManagerType) (string, error) {
-// 	log.Info("Building image", imageName, "with", containerManagerType.String())
-// 	imageName = path.Join(*tests.ContainerRegistry, repo, imageName)
-// 	dockerFilePath := filepath.Join(filepath.FromSlash(tests.GetTestResourcesPath()), "docker")
-// 	imageBuilder := NewBuildDockerImage(imageName, dockerFilePath, containerManagerType).SetDockerFileName(dockerfileName)
-// 	return imageName, gofrogcmd.RunCmd(imageBuilder)
-// }
-
-// func ContainerTestCleanup(t *testing.T, serverDetails *config.ServerDetails, artHttpDetails httputils.HttpClientDetails, imageName, buildName, repo string) {
-// 	// Remove build from Artifactory
-// 	DeleteBuild(serverDetails.ArtifactoryUrl, buildName, artHttpDetails)
-// 	tests.CleanFileSystem()
-// 	// Remove image from Artifactory
-// 	deleteSpec := spec.NewBuilder().Pattern(path.Join(repo, imageName)).BuildSpec()
-// 	successCount, failCount, err := tests.DeleteFiles(deleteSpec, serverDetails)
-// 	assert.Greater(t, successCount, 0)
-// 	assert.Equal(t, failCount, 0)
-// 	assert.NoError(t, err)
-// }
-
-// func getAllImagesNames(serverDetails *config.ServerDetails) ([]string, error) {
-// 	var imageNames []string
-// 	for _, repo := range []string{tests.DockerLocalRepo, tests.DockerLocalPromoteRepo} {
-// 		prefix := repo + "/"
-// 		specFile := spec.NewBuilder().Pattern(prefix + tests.DockerImageName + "*").IncludeDirs(true).BuildSpec()
-// 		searchCmd := generic.NewSearchCommand()
-// 		searchCmd.SetServerDetails(serverDetails).SetSpec(specFile)
-// 		reader, err := searchCmd.Search()
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		for searchResult := new(utils.SearchResult); reader.NextRecord(searchResult) == nil; searchResult = new(utils.SearchResult) {
-// 			imageNames = append(imageNames, strings.TrimPrefix(searchResult.Path, prefix))
-// 		}
-// 		err = reader.Close()
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 	}
-// 	return imageNames, nil
-// }
-
-// func CleanUpOldImages(serverDetails *config.ServerDetails) {
-// 	getActualItems := func() ([]string, error) { return getAllImagesNames(serverDetails) }
-// 	deleteItem := func(imageName string) {
-// 		deleteSpec := spec.NewBuilder().Pattern(path.Join(tests.DockerLocalRepo, imageName)).BuildSpec()
-// 		_, _, err := tests.DeleteFiles(deleteSpec, serverDetails)
-// 		if err != nil {
-// 			log.Error("Couldn't delete image", imageName, ":", imageName)
-// 		} else {
-// 			log.Info("Image", imageName, "deleted.")
-// 		}
-// 	}
-// 	tests.CleanUpOldItems([]string{tests.DockerImageName}, getActualItems, deleteItem)
-// }

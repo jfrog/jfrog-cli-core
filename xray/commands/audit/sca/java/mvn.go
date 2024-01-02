3e9c05164dfc2883da47cfb53a884a69bd40a5f5
Omer Zidkoni <omerz@jfrog.com>
2024-01-02 17:05:20 +0200

    Bump maven-dep-tree to 1.1.0

--- xray/commands/audit/sca/java/mvn.go
+++ xray/commands/audit/sca/java/mvn.go
@@ -20,7 +20,7 @@ const (
 	mavenDepTreeJarFile    = "maven-dep-tree.jar"
 	mavenDepTreeOutputFile = "mavendeptree.out"
 	// Changing this version also requires a change in MAVEN_DEP_TREE_VERSION within buildscripts/download_jars.sh
-	mavenDepTreeVersion = "1.0.2"
+	mavenDepTreeVersion = "1.1.0"
 	settingsXmlFile     = "settings.xml"
 )
 

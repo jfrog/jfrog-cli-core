2653d8805fcc9e14de28a583a97145951724a67e
Eran Turgeman <81029514+eranturgeman@users.noreply.github.com>
2024-01-16 09:45:00 +0200

    Improve recursive scan detection (#1105)

--- xray/commands/audit/audit.go
+++ xray/commands/audit/audit.go
@@ -83,10 +83,14 @@ func (auditCmd *AuditCommand) CreateXrayGraphScanParams() *services.XrayGraphSca
 }
 
 func (auditCmd *AuditCommand) Run() (err error) {
+	// If no workingDirs were provided by the user, we apply a recursive scan on the root repository
+	isRecursiveScan := len(auditCmd.workingDirs) == 0
+
 	workingDirs, err := coreutils.GetFullPathsWorkingDirs(auditCmd.workingDirs)
 	if err != nil {
 		return
 	}
+
 	auditParams := NewAuditParams().
 		SetXrayGraphScanParams(auditCmd.CreateXrayGraphScanParams()).
 		SetWorkingDirs(workingDirs).
@@ -94,7 +98,8 @@ func (auditCmd *AuditCommand) Run() (err error) {
 		SetFixableOnly(auditCmd.fixableOnly).
 		SetGraphBasicParams(auditCmd.AuditBasicParams).
 		SetThirdPartyApplicabilityScan(auditCmd.thirdPartyApplicabilityScan).
-		SetExclusions(auditCmd.exclusions)
+		SetExclusions(auditCmd.exclusions).
+		SetIsRecursiveScan(isRecursiveScan)
 	auditResults, err := RunAudit(auditParams)
 	if err != nil {
 		return

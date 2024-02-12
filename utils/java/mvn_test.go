28694a806b6cd7cf10391813ddb825265a89a27c
asafambar <asafa@jfrog.com>
2024-02-12 12:30:36 +0200

    fix test.

--- utils/java/mvn_test.go
+++ utils/java/mvn_test.go
@@ -36,6 +36,7 @@ const (
         </mirror>
     </mirrors>
 </settings>`
+	//#nosec G101 - dummy token for testing
 	settingsXmlWithUsernameAndPasswordAndCurationDedicatedAPi = `<?xml version="1.0" encoding="UTF-8"?>
 <settings xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.2.0 http://maven.apache.org/xsd/settings-1.2.0.xsd"
           xmlns="http://maven.apache.org/SETTINGS/1.2.0"

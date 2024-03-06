15eda5fe50e25de5ba2fbc50b2bea96f4f737b42
delarea <eyalde@jfrog.com>
2024-03-06 15:45:37 +0200

    Add publish test for multiple files

--- artifactory/commands/npm/publish_test.go
+++ artifactory/commands/npm/publish_test.go
@@ -9,12 +9,17 @@ import (
 
 func TestReadPackageInfoFromTarball(t *testing.T) {
 	npmPublish := NewNpmPublishCommand()
-	for index, file := range npmPublish.packedFilesPath {
-		npmPublish.packedFilesPath[index] = filepath.Join("..", "testdata", "npm", "npm-example-0.0.3.tgz")
-		err := npmPublish.readPackageInfoFromTarball(file)
-		assert.NoError(t, err)
+	npmPublish.packedFilesPath = append(npmPublish.packedFilesPath, filepath.Join("..", "testdata", "npm", "npm-example-0.0.3.tgz"))
+	npmPublish.packedFilesPath = append(npmPublish.packedFilesPath, filepath.Join("..", "testdata", "npm", "npm-example-0.0.4.tgz"))
 
-		assert.Equal(t, "npm-example", npmPublish.packageInfo.Name)
-		assert.Equal(t, "0.0.3", npmPublish.packageInfo.Version)
-	}
+	err := npmPublish.readPackageInfoFromTarball(npmPublish.packedFilesPath[0])
+	assert.NoError(t, err)
+	assert.Equal(t, "npm-example", npmPublish.packageInfo.Name)
+	assert.Equal(t, "0.0.3", npmPublish.packageInfo.Version)
+
+	err = npmPublish.readPackageInfoFromTarball(npmPublish.packedFilesPath[1])
+	assert.NoError(t, err)
+	assert.Equal(t, "npm-example", npmPublish.packageInfo.Name)
+	assert.Equal(t, "0.0.4", npmPublish.packageInfo.Version)
+
 }

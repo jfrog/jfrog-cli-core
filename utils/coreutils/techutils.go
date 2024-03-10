5fffc07d4b76bdd0cb440af81c2d4c74c657f1b0
Eran Turgeman <erant@jfrog.com>
2024-03-10 12:34:38 +0200

    updated techutils to fit new support in PNPM in Frogbot

--- utils/coreutils/techutils.go
+++ utils/coreutils/techutils.go
@@ -87,11 +87,12 @@ var technologiesData = map[Technology]TechData{
 		applicabilityScannable:     true,
 	},
 	Pnpm: {
-		indicators:             []string{"pnpm-lock.yaml"},
-		exclude:                []string{".yarnrc.yml", "yarn.lock", ".yarn"},
-		packageDescriptors:     []string{"package.json"},
-		packageVersionOperator: "@",
-		applicabilityScannable: true,
+		indicators:                 []string{"pnpm-lock.yaml"},
+		exclude:                    []string{".yarnrc.yml", "yarn.lock", ".yarn"},
+		packageDescriptors:         []string{"package.json"},
+		packageVersionOperator:     "@",
+		packageInstallationCommand: "update", // TODO ERAN check if any test needs to be updated following this change
+		applicabilityScannable:     true,
 	},
 	Yarn: {
 		indicators:             []string{".yarnrc.yml", "yarn.lock", ".yarn", ".yarnrc"},

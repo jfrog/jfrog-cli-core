c9d37914a05056a9301cbc28a2b37fe0c14d077e
delarea <eyalde@jfrog.com>
2024-05-05 13:37:20 +0300

    Add tests

---
+++ artifactory/utils/githubjobsummary_test.go
@@ -0,0 +1,56 @@
+package utils
+
+import (
+	buildinfo "github.com/jfrog/build-info-go/entities"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGenerateGitHubActionSummary(t *testing.T) {
+	// TODO: Integration test with local file system
+}
+
+func TestBuildUiUrl(t *testing.T) {
+	gh := &GitHubActionSummary{
+		platformUrl:     "https://myplatform.com/",
+		jfrogProjectKey: "myProject",
+	}
+	expected := "https://myplatform.com/ui/repos/tree/General/myPath/?projectKey=myProject"
+	actual := gh.buildUiUrl("myPath")
+	assert.Equal(t, expected, actual)
+
+	gh = &GitHubActionSummary{
+		platformUrl:     "https://myplatform.com/",
+		jfrogProjectKey: "",
+	}
+	expected = "https://myplatform.com/ui/repos/tree/General/myPath/?projectKey="
+	actual = gh.buildUiUrl("myPath")
+	assert.Equal(t, expected, actual)
+}
+
+func TestBuildInfoTable(t *testing.T) {
+	gh := &GitHubActionSummary{
+		platformUrl:     "https://myplatform.com/",
+		jfrogProjectKey: "",
+	}
+	gh.publishedBuildInfo = []*buildinfo.BuildInfo{
+		{
+			Name:     "buildName",
+			Number:   "123",
+			Started:  "2024-05-05T12:47:20.803+0300",
+			BuildUrl: "http://myJFrogPlatform/builds/buildName/123",
+		},
+	}
+	expected := "| 🔢 Build | 🕒 Timestamp | \n|---------|------------| \n| [buildName / 123](http://myJFrogPlatform/builds/buildName/123) | May 5, 2024 12:47:20 |\n"
+	assert.Equal(t, expected, gh.buildInfoTable())
+}
+
+func TestParseBuildTime(t *testing.T) {
+	expected := "Jan 2, 2006 15:04:05"
+	actual := parseBuildTime("2006-01-02T15:04:05.000-0700")
+	assert.Equal(t, expected, actual)
+
+	expected = "N/A"
+	actual = parseBuildTime("")
+	assert.Equal(t, expected, actual)
+}

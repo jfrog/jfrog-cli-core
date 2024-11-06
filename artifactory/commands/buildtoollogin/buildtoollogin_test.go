265fea7d94dea867ade15513ab1488c5e35f1a74
Michael Sverdlov <michaelsv@jfrog.com>
2024-11-06 14:25:11 +0200

    Improve repositores code
    Signed-off-by: Michael Sverdlov <michaelsv@jfrog.com>

--- artifactory/commands/buildtoollogin/buildtoollogin_test.go
+++ artifactory/commands/buildtoollogin/buildtoollogin_test.go
@@ -268,7 +268,7 @@ func TestBuildToolLoginCommand_configurePoetry(t *testing.T) {
 			assert.NoError(t, err)
 			poetryAuthContent := string(poetryAuthContentBytes)
 			// Normalize line endings for comparison.(For Windows)
-			poetryConfigContent = strings.ReplaceAll(poetryConfigContent, "\r\n", "\n")
+			poetryAuthContent = strings.ReplaceAll(poetryAuthContent, "\r\n", "\n")
 
 			if testCase.accessToken != "" {
 				// Validate token-based authentication (The token is stored in the keyring so we can't test it)

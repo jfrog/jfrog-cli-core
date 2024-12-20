3fc3c0e491cac85ef25e2cb6bacfc83bc0e12f60
eyalbe4 <eyalb@jfrog.com>
2024-12-20 23:22:19 +0200

    Unit test

---
+++ utils/usage/visibility_system_manager_test.go
@@ -0,0 +1,52 @@
+package usage_test
+
+import (
+	"testing"
+
+	"github.com/jfrog/jfrog-cli-core/v2/utils/coreutils"
+	"github.com/jfrog/jfrog-cli-core/v2/utils/testsutils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateMetric(t *testing.T) {
+	// Set environment variables for the test using SetEnvWithCallbackAndAssert
+	envVars := map[string]string{
+		"JFROG_CLI_USAGE_OIDC_USED":                                  "true",
+		"JFROG_CLI_USAGE_JOB_ID":                                     "job123",
+		"JFROG_CLI_USAGE_RUN_ID":                                     "run456",
+		"JFROG_CLI_USAGE_GIT_REPO":                                   "test-repo",
+		"JFROG_CLI_USAGE_GH_TOKEN_FOR_CODE_SCANNING_ALERTS_PROVIDED": "true",
+	}
+	cleanupFuncs := []func(){}
+	for key, value := range envVars {
+		cleanup := testsutils.SetEnvWithCallbackAndAssert(t, key, value)
+		cleanupFuncs = append(cleanupFuncs, cleanup)
+	}
+	defer func() {
+		for _, cleanup := range cleanupFuncs {
+			cleanup()
+		}
+	}()
+
+	commandName := "testCommand"
+	metric, err := createMetric(commandName)
+	assert.NoError(t, err)
+
+	// Define the expected JSON structure
+	expectedJSON := `{
+		"value": 1,
+		"metrics_name": "jfcli_commands_countaa",
+		"labels": {
+			"product_id": "` + coreutils.GetCliUserAgentName() + `",
+			"feature_id": "testCommand",
+			"oidc_used": "true",
+			"job_id": "job123",
+			"run_id": "run456",
+			"git_repo": "https://github.com/jfrog/test-repo",
+			"gh_token_for_code_scanning_alerts_provided": "true"
+		}
+	}`
+
+	// Compare the generated JSON to the expected JSON
+	assert.JSONEq(t, expectedJSON, string(metric))
+}

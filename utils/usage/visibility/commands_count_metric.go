10d99d86678a762dc5aefcec473645a220eb8a8e
Omer Zidkoni <50792403+omerzi@users.noreply.github.com>
2025-01-16 11:30:41 +0200

    CR

--- utils/usage/visibility/commands_count_metric.go
+++ utils/usage/visibility/commands_count_metric.go
@@ -8,9 +8,18 @@ import (
 
 func newCommandsCountMetric(commandName string) services.VisibilityMetric {
 	return services.VisibilityMetric{
-		Value:  1,
-		Name:   "jfcli_commands_count",
-		Labels: newCommandsCountLabels(commandName),
+		Value: 1,
+		Name:  "jfcli_commands_count",
+		Labels: &commandsCountLabels{
+			ProductID:                            coreutils.GetCliUserAgentName(),
+			ProductVersion:                       coreutils.GetCliUserAgentVersion(),
+			FeatureID:                            commandName,
+			OIDCUsed:                             os.Getenv("JFROG_CLI_USAGE_OIDC_USED"),
+			JobID:                                os.Getenv("JFROG_CLI_USAGE_JOB_ID"),
+			RunID:                                os.Getenv("JFROG_CLI_USAGE_RUN_ID"),
+			GitRepo:                              os.Getenv("JFROG_CLI_USAGE_GIT_REPO"),
+			GhTokenForCodeScanningAlertsProvided: os.Getenv("JFROG_CLI_USAGE_GH_TOKEN_FOR_CODE_SCANNING_ALERTS_PROVIDED"),
+		},
 	}
 }
 
@@ -23,17 +32,4 @@ type commandsCountLabels struct {
 	RunID                                string `json:"run_id"`
 	GitRepo                              string `json:"git_repo"`
 	GhTokenForCodeScanningAlertsProvided string `json:"gh_token_for_code_scanning_alerts_provided"`
-}
-
-func newCommandsCountLabels(featureID string) *commandsCountLabels {
-	return &commandsCountLabels{
-		ProductID:                            coreutils.GetCliUserAgentName(),
-		ProductVersion:                       coreutils.GetCliUserAgentVersion(),
-		FeatureID:                            featureID,
-		OIDCUsed:                             os.Getenv("JFROG_CLI_USAGE_OIDC_USED"),
-		JobID:                                os.Getenv("JFROG_CLI_USAGE_JOB_ID"),
-		RunID:                                os.Getenv("JFROG_CLI_USAGE_RUN_ID"),
-		GitRepo:                              os.Getenv("JFROG_CLI_USAGE_GIT_REPO"),
-		GhTokenForCodeScanningAlertsProvided: os.Getenv("JFROG_CLI_USAGE_GH_TOKEN_FOR_CODE_SCANNING_ALERTS_PROVIDED"),
-	}
 }

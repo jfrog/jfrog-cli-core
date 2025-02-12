08655e608132e101d587f7a6f0227a5a04477ce4
Bhanu Reddy <bhanur@jfrog.com>
2025-02-12 07:41:26 +0530

    Migrating rt flags

---
+++ common/cliutils/spec.go
@@ -0,0 +1,81 @@
+package cliutils
+
+import (
+	speccore "github.com/jfrog/jfrog-cli-core/v2/common/spec"
+	"github.com/jfrog/jfrog-cli-core/v2/plugins/components"
+	"github.com/jfrog/jfrog-cli-core/v2/utils/coreutils"
+	"strings"
+)
+
+func GetSpec(c *components.Context, isDownload, overrideFieldsIfSet bool) (specFiles *speccore.SpecFiles, err error) {
+	specFiles, err = speccore.CreateSpecFromFile(c.GetStringFlagValue("spec"), coreutils.SpecVarsStringToMap(c.GetStringFlagValue("spec-vars")))
+	if err != nil {
+		return nil, err
+	}
+	if isDownload {
+		trimPatternPrefix(specFiles)
+	}
+	if overrideFieldsIfSet {
+		overrideSpecFields(c, specFiles)
+	}
+	return
+}
+
+func overrideSpecFields(c *components.Context, specFiles *speccore.SpecFiles) {
+	for i := 0; i < len(specFiles.Files); i++ {
+		OverrideFieldsIfSet(specFiles.Get(i), c)
+	}
+}
+
+func trimPatternPrefix(specFiles *speccore.SpecFiles) {
+	for i := 0; i < len(specFiles.Files); i++ {
+		specFiles.Get(i).Pattern = strings.TrimPrefix(specFiles.Get(i).Pattern, "/")
+	}
+}
+
+func OverrideFieldsIfSet(spec *speccore.File, c *components.Context) {
+	overrideArrayIfSet(&spec.Exclusions, c, "exclusions")
+	overrideArrayIfSet(&spec.SortBy, c, "sort-by")
+	overrideIntIfSet(&spec.Offset, c, "offset")
+	overrideIntIfSet(&spec.Limit, c, "limit")
+	overrideStringIfSet(&spec.SortOrder, c, "sort-order")
+	overrideStringIfSet(&spec.Props, c, "props")
+	overrideStringIfSet(&spec.TargetProps, c, "target-props")
+	overrideStringIfSet(&spec.ExcludeProps, c, "exclude-props")
+	overrideStringIfSet(&spec.Build, c, "build")
+	overrideStringIfSet(&spec.Project, c, "project")
+	overrideStringIfSet(&spec.ExcludeArtifacts, c, "exclude-artifacts")
+	overrideStringIfSet(&spec.IncludeDeps, c, "include-deps")
+	overrideStringIfSet(&spec.Bundle, c, "bundle")
+	overrideStringIfSet(&spec.Recursive, c, "recursive")
+	overrideStringIfSet(&spec.Flat, c, "flat")
+	overrideStringIfSet(&spec.Explode, c, "explode")
+	overrideStringIfSet(&spec.BypassArchiveInspection, c, "bypass-archive-inspection")
+	overrideStringIfSet(&spec.Regexp, c, "regexp")
+	overrideStringIfSet(&spec.IncludeDirs, c, "include-dirs")
+	overrideStringIfSet(&spec.ValidateSymlinks, c, "validate-symlinks")
+	overrideStringIfSet(&spec.Symlinks, c, "symlinks")
+	overrideStringIfSet(&spec.Transitive, c, "transitive")
+	overrideStringIfSet(&spec.PublicGpgKey, c, "gpg-key")
+}
+
+// If `fieldName` exist in the cli args, read it to `field` as a string.
+func overrideStringIfSet(field *string, c *components.Context, fieldName string) {
+	if c.IsFlagSet(fieldName) {
+		*field = c.GetStringFlagValue(fieldName)
+	}
+}
+
+// If `fieldName` exist in the cli args, read it to `field` as an array split by `;`.
+func overrideArrayIfSet(field *[]string, c *components.Context, fieldName string) {
+	if c.IsFlagSet(fieldName) {
+		*field = append([]string{}, strings.Split(c.GetStringFlagValue(fieldName), ";")...)
+	}
+}
+
+// If `fieldName` exist in the cli args, read it to `field` as a int.
+func overrideIntIfSet(field *int, c *components.Context, fieldName string) {
+	if c.IsFlagSet(fieldName) {
+		*field, _ = c.GetIntFlagValue(fieldName)
+	}
+}
--- plugins/components/commandcomp.go
+++ plugins/components/commandcomp.go
@@ -171,6 +171,30 @@ func SetHiddenStrFlag() StringFlagOption {
 	}
 }
 
+func SetMandatoryFalse() StringFlagOption {
+	return func(f *StringFlag) {
+		f.Mandatory = false
+	}
+}
+
+func SetMandatoryTrue() StringFlagOption {
+	return func(f *StringFlag) {
+		f.Mandatory = true
+	}
+}
+
+func WithBoolDefaultValueFalse() BoolFlagOption {
+	return func(f *BoolFlag) {
+		f.DefaultValue = false
+	}
+}
+
+func WithBoolDefaultValueTrue() BoolFlagOption {
+	return func(f *BoolFlag) {
+		f.DefaultValue = true
+	}
+}
+
 type BoolFlag struct {
 	BaseFlag
 	DefaultValue bool
@@ -200,4 +224,18 @@ func SetHiddenBoolFlag() BoolFlagOption {
 	return func(f *BoolFlag) {
 		f.Hidden = true
 	}
+}
+
+func (c *Context) WithDefaultIntFlagValue(flagName string, defValue int) (value int, err error) {
+	value = defValue
+	if c.IsFlagSet(flagName) {
+		var parsed int64
+		parsed, err = strconv.ParseInt(c.GetStringFlagValue(flagName), 0, 64)
+		if err != nil {
+			err = fmt.Errorf("can't parse int flag '%s': %w", flagName, err)
+			return
+		}
+		value = int(parsed)
+	}
+	return
 }
